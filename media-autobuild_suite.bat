6098410c2b27858da0c5a967ce6e39d858d60795
Jonathan Baecker <jb@pixelcrusher.de>
2013-09-29 02:36:24 +0200

    add strip

--- media-autobuild_suite.bat
+++ media-autobuild_suite.bat
@@ -837,30 +837,8 @@ if %build64%==yes (
 	
 echo -------------------------------------------------------------------------------
 echo.
-echo.- striping all bins...
+echo.- compiling...
 echo.
 echo -------------------------------------------------------------------------------
-
-if %build32%==yes (
-	FOR /R "%instdir%\local32\bin" %%C IN (*.exe) DO (
-		%instdir%\mingw32\bin\strip --strip-all %%C 
-		echo.%%C done...
-		)
-	FOR /R "%instdir%\local32\bin" %%D IN (*.dll) DO (
-		%instdir%\mingw32\bin\strip --strip-all %%D 
-		echo.%%D done...
-		)
-	)
-	
-if %build64%==yes (
-	FOR /R "%instdir%\local64\bin" %%C IN (*.exe) DO (
-		%instdir%\mingw64\bin\strip --strip-all %%C 
-		echo.%%C done...
-		)
-	FOR /R "%instdir%\local64\bin" %%D IN (*.dll) DO (
-		%instdir%\mingw64\bin\strip --strip-all %%D 
-		echo.%%D done...
-		)
-	)
 
 pause
---
+++ strip_files.bat
@@ -0,0 +1,34 @@
+@echo off
+color 87
+title strip tools
+
+set instdir=%CD%
+
+if %build32%==yes (
+	FOR /R "%instdir%\local32\bin" %%C IN (*.exe) DO (
+		%instdir%\mingw32\bin\strip --strip-all %%C 
+		echo.%%C done...
+		)
+	FOR /R "%instdir%\local32\bin" %%D IN (*.dll) DO (
+		%instdir%\mingw32\bin\strip --strip-all %%D 
+		echo.%%D done...
+		)
+	)
+	
+if %build64%==yes (
+	FOR /R "%instdir%\local64\bin" %%C IN (*.exe) DO (
+		%instdir%\mingw64\bin\strip --strip-all %%C 
+		echo.%%C done...
+		)
+	FOR /R "%instdir%\local64\bin" %%D IN (*.dll) DO (
+		%instdir%\mingw64\bin\strip --strip-all %%D 
+		echo.%%D done...
+		)
+	)
+echo -------------------------------------------------------------------------------
+echo.
+echo.- striping all bins...
+echo.
+echo -------------------------------------------------------------------------------
+	
+pause
