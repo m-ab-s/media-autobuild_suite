1b9109b5299c9a0c988a11f3265bf2fe71fa8d8f
jb-alvarado <jb@pixelcrusher.de>
2013-11-13 16:44:43 +0100

    no webkit

--- wip(temp)/compile_vlc.sh
+++ wip(temp)/compile_vlc.sh
@@ -13,7 +13,7 @@ if [ -f "qt-everywhere-opensource-src-4.8.5/compile.done" ]; then
 		sed -i 's/QMAKE_LFLAGS		=/QMAKE_LFLAGS		= -static -static-libgcc -static-libstdc++/' "mkspecs/win32-g++/qmake.conf"
 		sed -i 's/LFLAGS      = -static-libgcc -s/LFLAGS      = -static -static-libgcc -static-libstdc++/' "qmake/Makefile.win32-g++"
 		
-		./configure.exe -prefix $LOCALDESTDIR/qt4 -static -release -opensource -confirm-license -platform win32-g++ -no-exceptions -no-qt3support
+		./configure.exe -prefix $LOCALDESTDIR/qt4 -static -release -opensource -confirm-license -platform win32-g++ -no-exceptions -no-qt3support -no-webkit
 		mingw32-make -j6
 		mingw32-make install
 		strip --strip-all $LOCALDESTDIR/qt4/bin/*.exe
