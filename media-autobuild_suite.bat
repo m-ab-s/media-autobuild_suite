41a7a887b3e98d5ef9e35169ebc841171368180a
Jonathan Baecker <jb@pixelcrusher.de>
2013-11-24 13:39:34 +0100

    fix download from compiler

--- media-autobuild_suite.bat
+++ media-autobuild_suite.bat
@@ -340,7 +340,7 @@ if %build32%==yes (
 		echo.
 		echo -------------------------------------------------------------------------------
 		if exist mingw32-gcc-4.8.0.7z GOTO instMingW32
-		%instdir%\msys\1.0\bin\wget.exe -c -O mingw32-gcc-4.8.2.7z "https://downloads.sourceforge.net/project/mingw-w64/Toolchains targetting Win32/Personal Builds/mingw-builds/4.8.2/threads-posix/sjlj/i686-4.8.2-release-posix-sjlj-rt_v3-rev0.7z"
+		%instdir%\msys\1.0\bin\wget.exe -c --no-check-certificate -O mingw32-gcc-4.8.2.7z "https://downloads.sourceforge.net/project/mingw-w64/Toolchains targetting Win32/Personal Builds/mingw-builds/4.8.2/threads-posix/sjlj/i686-4.8.2-release-posix-sjlj-rt_v3-rev0.7z"
 
 		:instMingW32
 		%instdir%\opt\bin\7za.exe x mingw32-gcc-4.8.2.7z
@@ -361,7 +361,7 @@ if exist "%instdir%\mingw64\bin\gcc.exe" GOTO makeDIR
 	echo.
 	echo -------------------------------------------------------------------------------
 	if exist mingw64-gcc-4.8.0.7z GOTO instMingW64
-	%instdir%\msys\1.0\bin\wget.exe -c -O mingw64-gcc-4.8.2.7z "https://downloads.sourceforge.net/project/mingw-w64/Toolchains targetting Win64/Personal Builds/mingw-builds/4.8.2/threads-posix/sjlj/x86_64-4.8.2-release-posix-sjlj-rt_v3-rev0.7z"
+	%instdir%\msys\1.0\bin\wget.exe -c --no-check-certificate -O mingw64-gcc-4.8.2.7z "https://downloads.sourceforge.net/project/mingw-w64/Toolchains targetting Win64/Personal Builds/mingw-builds/4.8.2/threads-posix/sjlj/x86_64-4.8.2-release-posix-sjlj-rt_v3-rev0.7z"
 	
 	:instMingW64
 	%instdir%\opt\bin\7za.exe x mingw64-gcc-4.8.2.7z
