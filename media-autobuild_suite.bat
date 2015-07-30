7acb850ea79c1f7c442acf70e276b8218312a36b
RiCON <RiCON@nowhere>
2015-07-31 00:23:51 +0100

    Remove cloog again
    If we keep it for GCC 5.2 it prevents installation of other packages.
    Since GCC 4.9.2 depends on it it won't get removed and will still
    get installed normally.

--- media-autobuild_suite.bat
+++ media-autobuild_suite.bat
@@ -37,7 +37,7 @@ automake1.14 automake1.6 automake1.7 automake1.8 automake1.9 autogen bison diffs
 intltool libtool patch pkg-config python scons xmlto make tar zip unzip git subversion wget p7zip mercurial rubygems ^
 man-db
 
-set mingwpackages=boost cloog cmake crt-git dlfcn doxygen gcc gcc-ada gcc-fortran gcc-libgfortran gcc-libs gcc-objc ^
+set mingwpackages=boost cmake crt-git dlfcn doxygen gcc gcc-ada gcc-fortran gcc-libgfortran gcc-libs gcc-objc ^
 gettext glew gmp gsm headers-git jasper lame lcms2 libcddb libcdio libgpg-error libiconv libjpeg libmodplug libpng ^
 libtiff mpc nasm pcre schroedinger sqlite3 tools-git winpthreads-git xvidcore yasm
 
