046d54f0a3467e98b8fbab691d1af4b18c0f494d
RiCON <RiCON@nowhere>
2015-08-04 22:38:55 +0100

    [libjpeg] Add --with-jpeg8 to configure

--- media-suite_compile.sh
+++ media-suite_compile.sh
@@ -522,7 +522,7 @@ if do_checkForOptions "--enable-libopenjpeg" && [[ $ffmbc = "n" ]]; then
         [[ -f configure ]] || autoreconf -fiv
         [[ -f Makefile ]] && make distclean
         do_patch "libjpegturbo-0001-Fix-header-conflicts-with-MinGW.patch"
-        do_generic_confmakeinstall global --without-turbojpeg
+        do_generic_confmakeinstall global --without-turbojpeg --with-jpeg8
         do_checkIfExist libjpegturbo libjpeg.a
     fi
 
