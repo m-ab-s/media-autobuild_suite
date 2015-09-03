7b5adf6d48bc6245ce05ffcfcfac51cc23ff0289
RiCON <RiCON@nowhere>
2015-09-03 20:06:37 +0100

    [pkgconfig] Fix #150
    With introduction of /trunk cygpath now uses it instead of $LOCALDESTDIR
    so we have to account for that too.

--- media-suite_compile.sh
+++ media-suite_compile.sh
@@ -297,7 +297,8 @@ do_pkgConfig() {
     local pkg=${1%% *}
     echo -ne "\033]0;compiling $pkg $bits\007"
     local prefix=$(pkg-config --variable=prefix --silence-errors "$1")
-    if [[ $(cygpath -u "$prefix") = "$LOCALDESTDIR" ]]; then
+    prefix="$(cygpath -u "$prefix")"
+    if [[ "$prefix" = "$LOCALDESTDIR" || "$prefix" = "/trunk${LOCALDESTDIR}" ]]; then
         echo -------------------------------------------------
         echo "$pkg is already compiled"
         echo -------------------------------------------------
