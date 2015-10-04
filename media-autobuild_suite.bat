71c487bdfe5cbbe4c0a55bb4458b3b2cd483143f
RiCON <RiCON@nowhere>
2015-10-04 17:43:46 +0100

    Install texinfo to build html documentation for ffmpeg and others

--- media-autobuild_suite.bat
+++ media-autobuild_suite.bat
@@ -50,7 +50,7 @@ if not exist %build% mkdir %build%
 set msyspackages=asciidoc autoconf autoconf2.13 automake-wrapper automake1.10 automake1.11 automake1.12 automake1.13 ^
 automake1.14 automake1.6 automake1.7 automake1.8 automake1.9 autogen bison diffstat diffutils dos2unix help2man ^
 intltool libtool patch python scons xmlto make tar zip unzip git subversion wget p7zip mercurial ruby man-db ^
-gperf winpty-git
+gperf winpty-git texinfo
 
 set mingwpackages=boost cmake crt-git dlfcn doxygen gcc gcc-ada gcc-fortran gcc-libgfortran gcc-libs gcc-objc ^
 gettext glew gmp headers-git jasper lcms2 libgpg-error libiconv libjpeg-turbo ^
