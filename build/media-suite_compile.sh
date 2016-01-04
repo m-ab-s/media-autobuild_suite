4eb850fbaf0899e796ca1d87d6fcaf175ae40a5a
RiCON <RiCON@nowhere>
2016-01-04 19:21:56 +0000

    ffmpeg: Update gcrypt patches

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -1204,8 +1204,8 @@ if [[ $ffmpeg != "n" ]]; then
         do_patch "ffmpeg-0001-Use-pkg-config-for-more-external-libs.patch" am
         do_patch "ffmpeg-0002-add-openhevc-intrinsics.patch" am
         do_checkForOptions "--enable-gcrypt" &&
-            do_patch "ffmpeg-0003-rtmpdh-Initialize-gcrypt-before-using-it.patch" am &&
-            do_patch "ffmpeg-0004-configure-Use-libgcrypt-config-if-available.patch" am
+            do_patch "ffmpeg-0003-configure-Use-libgcrypt-config-if-available.patch" am &&
+            do_patch "ffmpeg-0004-rtmpdh-Initialize-gcrypt-before-using-it.patch" am
 
         rm -rf $LOCALDESTDIR/include/libav{codec,device,filter,format,util,resample}
         rm -rf $LOCALDESTDIR/include/lib{sw{scale,resample},postproc}
