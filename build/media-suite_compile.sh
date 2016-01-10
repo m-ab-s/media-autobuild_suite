m-ab-s/media-autobuild_suite

commit 0ddf36f43195f0fea9ca77f84651109de72378cc

ffmpeg: use arrays for configure options

build/media-suite_compile.sh
@@ -4,7 +4,7 @@ cpuCount=1
 compile="false"
 buildFFmpeg="false"
 newFfmpeg="no"
-FFMPEG_BASE_OPTS="--enable-avisynth --pkg-config-flags=--static"
+FFMPEG_BASE_OPTS=("--enable-avisynth" "--pkg-config-flags=--static")
 alloptions="$*"
 echo -e "\nBuild start: $(date +"%F %T %z")" >> "$LOCALBUILDDIR"/newchangelog
 echo -ne "\e]0;media-autobuild_suite\007"

build/media-suite_helper.sh
@@ -253,16 +253,16 @@ do_getFFmpegConfig() {
     [[ -z "$license" && -n "$1" ]] && local license="$1"
     configfile="$LOCALBUILDDIR"/ffmpeg_options.txt
     if [[ -f "$configfile" ]] && [[ $ffmpegChoice != "n" ]]; then
-        FFMPEG_DEFAULT_OPTS=$(sed -e 's:\\::g' -e 's/#.*//' "$configfile" | tr '\n' ' ')
+        FFMPEG_DEFAULT_OPTS=($(sed -e 's:\\::g' -e 's/#.*//' "$configfile" | tr '\n' ' '))
         echo "Imported FFmpeg options from ffmpeg_options.txt"
     elif [[ -f "/trunk/media-autobuild_suite.bat" ]] && [[ $ffmpegChoice != "y" ]]; then
-        FFMPEG_DEFAULT_OPTS=$(sed -rne '/ffmpeg_options=/,/[^^]$/p' /trunk/media-autobuild_suite.bat | \
-            sed -e 's/.*ffmpeg_options=//' -e 's/ ^//g' | tr '\n' ' ')
+        FFMPEG_DEFAULT_OPTS=($(sed -rne '/ffmpeg_options=/,/[^^]$/p' /trunk/media-autobuild_suite.bat | \
+            sed -e 's/.*ffmpeg_options=//' -e 's/ ^//g' | tr '\n' ' '))
         echo "Imported default FFmpeg options from .bat"
     else
         echo "Using default FFmpeg options"
     fi
-    FFMPEG_OPTS="$FFMPEG_BASE_OPTS $FFMPEG_DEFAULT_OPTS"
+    FFMPEG_OPTS=("${FFMPEG_BASE_OPTS[@]}" "${FFMPEG_DEFAULT_OPTS[@]}")
 
     if [[ $bits = "32bit" ]]; then
         arch=x86
@@ -297,8 +297,8 @@ do_getFFmpegConfig() {
 
     # handle WinXP-incompatible libs
     if [[ $xpcomp = "y" ]]; then
-        do_removeOptions "--enable-libmfx --enable-decklink --enable-tesseract \
-            --enable-opencl --enable-libcaca"
+        do_removeOptions --enable-libmfx --enable-decklink --enable-tesseract \
+            --enable-opencl --enable-libcaca
     fi
 }
 
@@ -308,8 +308,8 @@ do_changeFFmpegConfig() {
     # decklink depends on pthreads
     if do_checkForOptions "--disable-w32threads --enable-pthreads --enable-decklink"; then
         do_removeOption "--enable-w32threads"
-        do_addOptions "--disable-w32threads --extra-cflags=-DPTW32_STATIC_LIB \
-            --extra-libs=-lpthread --extra-libs=-lwsock32"
+        do_addOptions --disable-w32threads --extra-cflags=-DPTW32_STATIC_LIB \
+            --extra-libs=-lpthread --extra-libs=-lwsock32
     fi
 
     # add options for static kvazaar
@@ -328,7 +328,7 @@ do_changeFFmpegConfig() {
     fi
 
     # handle (l)gplv3 libs
-    local version3="--enable-libopencore-amrwb --enable-libopencore-amrnb \
+    local version3="--enable-libopencore-amr(wb|nb) \
         --enable-libvo-aacenc --enable-libvo-amrwbenc --enable-gmp"
     if [[ $license = *v3 || $license = nonfree ]] && do_checkForOptions "$version3"; then
         do_addOption "--enable-version3"
@@ -372,51 +372,48 @@ do_changeFFmpegConfig() {
 
     # remove libs that don't work with shared
     if [[ $ffmpeg = "s" || $ffmpeg = "b" ]]; then
-        FFMPEG_OPTS_SHARED=$FFMPEG_OPTS
+        FFMPEG_OPTS_SHARED=("${FFMPEG_OPTS[@]}")
         do_removeOptions "--enable-decklink --enable-libutvideo --enable-libgme" y
-        FFMPEG_OPTS_SHARED="$FFMPEG_OPTS_SHARED --extra-ldflags=-static-libgcc"
+        FFMPEG_OPTS_SHARED+=("--extra-ldflags=-static-libgcc")
     fi
 }
 
 do_checkForOptions() {
-    local option=
-    local option2=
-    for option in "$@"; do
-        for option2 in $option; do
-            if /usr/bin/grep -qE -e "$option2" <(echo "$FFMPEG_OPTS"); then
-                return 0
-            fi
-        done
+    local option
+    for option in $@; do
+        if /usr/bin/grep -qE -e "$option" <(echo "${FFMPEG_OPTS[*]}"); then
+            return
+        fi
     done
     return 1
 }
 
 do_addOption() {
-    local option=${1%% *}
+    local option="$1"
     if ! do_checkForOptions "$option"; then
-        FFMPEG_OPTS="$FFMPEG_OPTS $option"
+        FFMPEG_OPTS+=("$option")
     fi
 }
 
 do_addOptions() {
-    local option=
-    for option in $1; do
+    local option
+    for option in "$@"; do
         do_addOption "$option"
     done
 }
 
 do_removeOption() {
-    local option=${1%% *}
+    local option=$1
     local shared=$2
     if [[ $shared = "y" ]]; then
-        FFMPEG_OPTS_SHARED=$(echo "$FFMPEG_OPTS_SHARED" | sed -r "s/ *$option//g")
+        FFMPEG_OPTS_SHARED=($(echo "${FFMPEG_OPTS_SHARED[*]}" | sed -r "s/ *$option//g"))
     else
-        FFMPEG_OPTS=$(echo "$FFMPEG_OPTS" | sed -r "s/ *$option//g")
+        FFMPEG_OPTS=($(echo "${FFMPEG_OPTS[*]}" | sed -r "s/ *$option//g"))
     fi
 }
 
 do_removeOptions() {
-    local option=
+    local option
     local shared=$2
     for option in $1; do
         do_removeOption "$option" "$shared"

commit bd695b8584fc1e93e82070286443c4839f2c24ac

do_(get|change)FFmpegConfig: use arrays for license checks
Regression since 0ddf36f4
Close #245

build/media-suite_compile.sh
@@ -52,7 +52,7 @@ echo -e "\n\t${orange_color}Starting $bits compilation of all tools${reset_color
 
 cd_safe "$LOCALBUILDDIR"
 
-do_getFFmpegConfig
+do_getFFmpegConfig $license
 do_getMpvConfig
 if [[ -n "$alloptions" ]]; then
     thisrun="$(printf '%s\n' '#!/bin/bash' "FFMPEG_DEFAULT_OPTS=\"${FFMPEG_DEFAULT_OPTS[*]}\"" \
@@ -1204,7 +1204,7 @@ if [[ $ffmpeg != "n" ]]; then
     do_hide_all_sharedlibs
 
     cd_safe "$LOCALBUILDDIR"
-    do_changeFFmpegConfig
+    do_changeFFmpegConfig $license
     do_vcs "http://source.ffmpeg.org/git/ffmpeg.git" ffmpeg bin-video/ffmpeg.exe
     if [[ $compile = "true" ]] || [[ $buildFFmpeg = "true" && $ffmpegUpdate = "y" ]] ||
         [[ $ffmpeg = "s" && ! -f $LOCALDESTDIR/bin-video/ffmpegSHARED/ffmpeg.exe ]]; then

build/media-suite_helper.sh
@@ -250,7 +250,7 @@ do_pkgConfig() {
 }
 
 do_getFFmpegConfig() {
-    [[ -z "$license" && -n "$1" ]] && local license="$1"
+    local license="$1"
     local configfile="$LOCALBUILDDIR"/ffmpeg_options.txt
     if [[ -f "$configfile" ]] && [[ $ffmpegChoice != "n" ]]; then
         FFMPEG_DEFAULT_OPTS=($(sed -e 's:\\::g' -e 's/#.*//' "$configfile" | tr '\n' ' '))
@@ -262,6 +262,7 @@ do_getFFmpegConfig() {
     else
         echo "Using default FFmpeg options"
     fi
+    echo "License: $license"
     FFMPEG_OPTS=("${FFMPEG_BASE_OPTS[@]}" "${FFMPEG_DEFAULT_OPTS[@]}")
 
     if [[ $bits = "32bit" ]]; then
@@ -303,7 +304,9 @@ do_getFFmpegConfig() {
 }
 
 do_changeFFmpegConfig() {
-    [[ -z "$license" && -n "$1" ]] && local license="$1"
+    local license="$1"
+    echo "Checking and changing FFmpeg options"
+    echo "License: $license"
     # if w32threads is disabled, pthreads is used and needs this cflag
     # decklink depends on pthreads
     if do_checkForOptions --disable-w32threads --enable-pthreads --enable-decklink; then
@@ -318,39 +321,39 @@ do_changeFFmpegConfig() {
     fi
 
     # handle gpl libs
-    local gpl="--enable-frei0r --enable-libcdio --enable-librubberband \
-        --enable-libutvideo --enable-libvidstab --enable-libx264 --enable-libx265 \
-        --enable-libxavs --enable-libxvid --enable-libzvbi"
-    if [[ $license = gpl* || $license = nonfree ]] && do_checkForOptions "$gpl"; then
+    local gpl=(--enable-frei0r --enable-libcdio --enable-librubberband
+        --enable-libutvideo --enable-libvidstab --enable-libx264 --enable-libx265
+        --enable-libxavs --enable-libxvid --enable-libzvbi)
+    if [[ $license = gpl* || $license = nonfree ]] && do_checkForOptions "${gpl[@]}"; then
         do_addOption "--enable-gpl"
     else
-        do_removeOptions "$gpl --enable-gpl"
+        do_removeOptions "${gpl[*]} --enable-gpl"
     fi
 
     # handle (l)gplv3 libs
-    local version3="--enable-libopencore-amr(wb|nb) \
-        --enable-libvo-aacenc --enable-libvo-amrwbenc --enable-gmp"
-    if [[ $license = *v3 || $license = nonfree ]] && do_checkForOptions "$version3"; then
+    local version3=("--enable-libopencore-amr(wb|nb)"
+        --enable-libvo-aacenc --enable-libvo-amrwbenc --enable-gmp)
+    if [[ $license = *v3 || $license = nonfree ]] && do_checkForOptions "${version3[@]}"; then
         do_addOption "--enable-version3"
     else
-        do_removeOptions "$version3 --enable-version3"
+        do_removeOptions "${version3[*]} --enable-version3"
     fi
 
     # handle non-free libs
-    local nonfree="--enable-nvenc --enable-libfaac"
-    if [[ $license = "nonfree" ]] && do_checkForOptions "$nonfree"; then
+    local nonfree=(--enable-nvenc --enable-libfaac)
+    if [[ $license = "nonfree" ]] && do_checkForOptions "${nonfree[@]}"; then
         do_addOption "--enable-nonfree"
     else
-        do_removeOptions "$nonfree --enable-nonfree"
+        do_removeOptions "${nonfree[*]} --enable-nonfree"
     fi
 
     # handle gpl-incompatible libs
-    local nonfreegpl="--enable-libfdk-aac --enable-openssl"
-    if do_checkForOptions "$nonfreegpl"; then
+    local nonfreegpl=(--enable-libfdk-aac --enable-openssl)
+    if do_checkForOptions "${nonfreegpl[@]}"; then
         if [[ $license = "nonfree" ]]; then
             do_addOption "--enable-nonfree"
         elif [[ $license = gpl* ]]; then
-            do_removeOptions "$nonfreegpl"
+            do_removeOptions "${nonfreegpl[*]}"
         fi
         # no lgpl here because they are accepted with it
     fi