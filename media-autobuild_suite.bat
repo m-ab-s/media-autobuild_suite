6bc469d1013d884afb564708d790fc86470d0de2
RiCON <RiCON@nowhere>
2016-01-16 16:45:21 +0000

    .bat: Replace gmp with gcrypt in default FFmpeg opts
    Gcrypt issues were fixed so it's ok to use instead.

--- media-autobuild_suite.bat
+++ media-autobuild_suite.bat
@@ -61,7 +61,7 @@ set ffmpeg_options=--enable-gnutls --enable-frei0r --enable-libbluray --enable-l
 --enable-libzvbi --enable-libdcadec --enable-libbs2b --enable-libmfx --enable-libcdio --enable-libfreetype ^
 --enable-fontconfig --enable-libfribidi --enable-opengl --enable-libvpx --enable-libx264 --enable-libx265 ^
 --enable-libkvazaar --enable-libwebp --enable-decklink --enable-libgme --enable-librubberband ^
---disable-w32threads --enable-opencl --enable-libzimg --enable-gmp ^
+--disable-w32threads --enable-opencl --enable-libzimg --enable-gcrypt ^
 --enable-nonfree --enable-nvenc --enable-openssl
 
 set mpv_options=--enable-dvdread --enable-dvdnav --enable-libbluray --enable-libass --enable-rubberband ^
