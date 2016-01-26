4d1f7a2af4b71b8a6a88795a3ecaed649c9e1aa6
RiCON <RiCON@nowhere>
2016-01-26 02:56:04 +0000

    dvdread: Use do_separate_confmakeinstall

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -653,9 +653,9 @@ if [[ $mplayer = "y" ]] || ! mpv_disabled_all dvdread dvdnav; then
     if [[ $compile = "true" ]]; then
         _check=(libdvdread.{l,}a dvdread.pc)
         do_autoreconf
-        [[ -f Makefile ]] && log "distclean" make distclean
         do_uninstall include/dvdread "${_check[@]}"
-        do_generic_confmakeinstall
+        [[ -f Makefile ]] && log "distclean" make distclean
+        do_separate_confmakeinstall
         do_checkIfExist "${_check[@]}"
     fi
     grep -q 'ldl' "$LOCALDESTDIR"/lib/pkgconfig/dvdread.pc ||
