m-ab-s/media-autobuild_suite

commit c31a913474a01229bcec64043663bc85005f18eb

More shellcheck.net corrections

build/media-suite_compile.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+shopt -s extglob
 
 buildFFmpeg="false"
 newFfmpeg="no"
@@ -52,17 +53,17 @@ echo -e "\n\t${orange_color}Starting $bits compilation of all tools${reset_color
 
 cd_safe "$LOCALBUILDDIR"
 
-do_getFFmpegConfig $license
+do_getFFmpegConfig "$license"
 do_getMpvConfig
 if [[ -n "$alloptions" ]]; then
-    thisrun="$(printf '%s\n' '#!/bin/bash' "FFMPEG_DEFAULT_OPTS=\"${FFMPEG_DEFAULT_OPTS[*]}\"" \
-            "MPV_OPTS=\"${MPV_OPTS[*]}\"" \
-            "bash $LOCALBUILDDIR/media-suite_compile.sh $alloptions")"
-    [[ -f "$LOCALBUILDDIR/last_successful_run" ]] &&
-        { diff -q <(echo "$thisrun") "$LOCALBUILDDIR/last_successful_run" >/dev/null 2>&1 ||
-            buildFFmpeg="true"; }
-    echo "$thisrun" > "$LOCALBUILDDIR/last_run"
-    unset alloptions thisrun
+    {
+        echo '#!/bin/bash'
+        echo 'FFMPEG_DEFAULT_OPTS=('
+        printf '\t"%s"\n' "${FFMPEG_DEFAULT_OPTS[@]}"
+        echo ')'
+        echo "bash $LOCALBUILDDIR/media-suite_compile.sh $alloptions"
+    } > "$LOCALBUILDDIR/last_run"
+    unset alloptions
 fi
 
 echo -e "\n\t${orange_color}Starting $bits compilation of global tools${reset_color}"
@@ -359,7 +360,7 @@ if { [[ $sox = "y" ]] || { [[ $ffmpeg != n ]] && enabled libspeex; }; } &&
     do_uninstall include/speex "${_check[@]}"
     do_patch speex-mingw-winmm.patch
     do_separate_confmakeinstall audio --enable-vorbis-psy \
-        $([[ $standalone = y ]] && echo --enable-binaries || echo --disable-binaries)
+        "$([[ $standalone = y ]] && echo --enable-binaries || echo --disable-binaries)"
     do_checkIfExist "${_check[@]}"
 fi
 
@@ -449,8 +450,8 @@ if [[ $standalone = y ]] && enabled libvorbis &&
     [[ -f Makefile ]] && log distclean make distclean
     do_separate_confmakeinstall audio --disable-ogg123 --disable-vorbiscomment \
         --disable-vcut --disable-ogginfo \
-        "$(enabled libspeex || echo "--without-speex")" \
-        "$([[ $flac = "y" ]] || echo "--without-flac")"
+        "$(enabled libspeex || echo --without-speex)" \
+        "$([[ $flac = "y" ]] || echo --without-flac)"
     do_checkIfExist "${_check[@]}"
     _to_remove+=($(pwd))
 fi
@@ -494,7 +495,7 @@ if enabled libmp3lame; then
         do_uninstall include/lame "${_check[@]}"
         [[ -f Makefile ]] && log distclean make distclean
         do_separate_confmakeinstall audio --disable-decoder \
-            $([[ $standalone = y ]] || echo "--disable-frontend")
+            "$([[ $standalone = y ]] || echo --disable-frontend)"
         do_checkIfExist "${_check[@]}"
     fi
 fi
@@ -567,7 +568,7 @@ if [[ $rtmpdump = "y" || $mediainfo = "y" ]] ||
     [[ $rtmpdump = "y" ]] && _check+=(bin-video/rtmpdump.exe)
     do_vcs "git://repo.or.cz/rtmpdump.git" librtmp "${_check[@]}"
     req=""
-    pc_exists librtmp && req="$(pkg-config --print-requires $LOCALDESTDIR/lib/pkgconfig/librtmp.pc)"
+    pc_exists librtmp && req="$(pkg-config --print-requires "$(file_installed librtmp.pc)")"
     if enabled gnutls || [[ $rtmpdump = "y" && $license != "nonfree" ]]; then
         crypto=GNUTLS
         pc=gnutls
@@ -700,7 +701,7 @@ if [[ $mplayer = "y" ]] || ! mpv_disabled libass ||
         do_autoreconf
         do_uninstall "${_check[@]}"
         [[ -f Makefile ]] && log "distclean" make distclean
-        do_separate_confmakeinstall $(enabled_any {lib,}fontconfig && echo --disable-fontconfig)
+        do_separate_confmakeinstall "$(enabled_any {lib,}fontconfig && echo --disable-fontconfig)"
         do_checkIfExist "${_check[@]}"
         buildFFmpeg="true"
         unset rebuildLibass
@@ -851,7 +852,7 @@ if [[ $ffmpeg != "n" ]] && enabled nvenc; then
             {nv{CPUOPSys,FileIO,Utils},NvHWEncoder}.h "${_check[0]}"
         mkdir -p "$LOCALBUILDDIR/NvEncAPI" &&
             cd_safe "$LOCALBUILDDIR/NvEncAPI"
-        do_wget -r -c -h "$_hash" "$LOCALBUILDDIR/extras/${_check[0]}"
+        do_wget -r -c -h "${_hash[0]}" "$LOCALBUILDDIR/extras/${_check[0]}"
         do_install "${_check[0]}" include/
         do_checkIfExist "${_check[0]}"
     fi
@@ -1046,7 +1047,6 @@ EOF
             _check+=(bin-video/x265-numa.exe)
         fi
         do_checkIfExist "${_check[@]}"
-        buildFFmpeg="true"
         unset xpsupport assembly cli
     fi
 else
@@ -1085,7 +1085,7 @@ if [[ $ffmpeg != "n" ]]; then
     fi
     do_vcs "http://source.ffmpeg.org/git/ffmpeg.git" ffmpeg "${_check[@]}"
     if [[ $compile = "true" ]] || [[ $buildFFmpeg = "true" && $ffmpegUpdate = "y" ]]; then
-        do_changeFFmpegConfig $license
+        do_changeFFmpegConfig "$license"
         enabled_any libgme libopencore-amr{nb,wb} libtheora libtwolame libvorbis libcdio &&
             do_patch "ffmpeg-0001-configure-Try-pkg-config-first-with-a-few-libs.patch" am
         do_patch "ffmpeg-0002-add-openhevc-intrinsics.patch" am
@@ -1116,7 +1116,7 @@ if [[ $ffmpeg != "n" ]]; then
                 fi
                 disabled ffprobe || _check+=(bin-video/ffmpegSHARED/bin/ffprobe.exe)
             fi
-            files_exist "${_check[@]}" && touch build_successful${bits}_shared
+            files_exist "${_check[@]}" && touch "build_successful${bits}_shared"
             [[ $ffmpeg = "b" ]] && mv ab-suite.{,shared.}configure.log &&
                 mv ab-suite.{,shared.}configure.error.log && mv ab-suite.{,shared.}install.log &&
                 mv ab-suite.{,shared.}install.error.log
@@ -1152,7 +1152,7 @@ if [[ $mplayer = "y" ]]; then
     _check=(bin-video/m{player,encoder}.exe)
     do_vcs "svn::svn://svn.mplayerhq.hu/mplayer/trunk" mplayer "${_check[@]}"
 
-    if [[ $compile == "true" ]] || [[ $buildFFmpeg == "true" ]]; then
+    if [[ $compile = "true" || $buildFFmpeg = "true" ]]; then
         do_uninstall "${_check[@]}"
         [[ -f config.mak ]] && log "distclean" make distclean
         if [[ ! -d ffmpeg ]]; then
@@ -1199,7 +1199,7 @@ if [[ $xpcomp = "n" && $mpv != "n" ]] && pc_exists libavcodec libavformat libsws
     [[ -d $LOCALBUILDDIR/waio-git ]] && do_uninstall include/waio libwaio.a &&
         _to_remove+=("$LOCALBUILDDIR/waio-git")
 
-    if ! mpv_disabled lua && [[ ${MPV_OPTS[@]} != ${MPV_OPTS[@]#--lua=lua51} ]]; then
+    if ! mpv_disabled lua && [[ ${MPV_OPTS[@]} != "${MPV_OPTS[@]#--lua=lua51}" ]]; then
         do_pacman_install lua51
     elif ! mpv_disabled lua; then
         do_pacman_remove lua51

build/media-suite_helper.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+shopt -q extglob; extglob_set=$?
+((extglob_set)) && shopt -s extglob
 
 if [[ ! $cpuCount =~ ^[0-9]+$ ]]; then
     cpuCount="$(($(nproc)/2))"
 fi
+bits="${bits:-64bit}"
 
 if which tput >/dev/null 2>&1; then
     ncolors=$(tput colors)
@@ -157,9 +160,9 @@ do_vcs() {
         do_print_status "┌ ${vcsFolder} ${vcsType}" "$orange_color" "Updates found"
     elif [[ -f recently_updated && ! -f "build_successful$bits" ]]; then
         do_print_status "┌ ${vcsFolder} ${vcsType}" "$orange_color" "Recently updated"
-    elif [[ -z "$vcsCheck" ]] && ! files_exist "$vcsFolder.pc"; then
+    elif [[ -z "${vcsCheck[@]}" ]] && ! files_exist "$vcsFolder.pc"; then
         do_print_status "┌ ${vcsFolder} ${vcsType}" "$orange_color" "Missing pkg-config"
-    elif [[ ! -z "$vcsCheck" ]] && ! files_exist "${vcsCheck[@]}"; then
+    elif [[ -n "${vcsCheck[@]}" ]] && ! files_exist "${vcsCheck[@]}"; then
         do_print_status "┌ ${vcsFolder} ${vcsType}" "$orange_color" "Files missing"
     else
         do_print_status "${vcsFolder} ${vcsType}" "$green_color" "Up-to-date"
@@ -168,7 +171,7 @@ do_vcs() {
 }
 
 guess_dirname() {
-    echo $(expr "$1" : '\(.\+\)\.\(tar\(\.\(gz\|bz2\|xz\)\)\?\|7z\|zip\)$')
+    expr "$1" : '\(.\+\)\.\(tar\(\.\(gz\|bz2\|xz\)\)\?\|7z\|zip\)$'
 }
 
 check_hash() {
@@ -209,7 +212,7 @@ do_wget() {
 
     [[ ! $nocd ]] && cd_safe "$LOCALBUILDDIR"
     if ! check_hash "$archive" "$hash"; then
-        [[ ${url#$LOCALBUILDDIR} != ${url} ]] &&
+        [[ ${url#$LOCALBUILDDIR} != "${url}" ]] &&
             url="https://raw.githubusercontent.com/jb-alvarado/media-autobuild_suite/master${url}"
 
         curlcmds=(curl --retry 20 --retry-max-time 5 -sLkf)
@@ -240,7 +243,7 @@ do_wget() {
         [[ $quiet ]] || do_print_status "├ ${dirName:-$archive}" "$green_color" "File up-to-date"
     fi
     [[ $norm ]] || _to_remove+=("$(pwd)/$archive")
-    do_extract $([[ $nocd ]] && echo nocd) "$archive" "$dirName"
+    do_extract "$([[ $nocd ]] && echo nocd)" "$archive" "$dirName"
     [[ ! $norm && $dirName && ! $nocd ]] && _to_remove+=("$(pwd)")
 }
 
@@ -287,25 +290,31 @@ do_extract() {
 
 do_wget_sf() {
     # do_wget_sf "faac/faac-src/faac-1.28/faac-$_ver.tar.bz2" "faac-$_ver"
-    local hash
+    local hash baseurl
     [[ $1 = "-h" ]] && hash="$2" && shift 2
-    local url="$1"
+    local url="http://download.sourceforge.net/$1"
     shift 1
-    do_wget $([[ $hash ]] && echo "-h $hash") \
-        "http://download.sourceforge.net/${url}" "$@"
+    if [[ -n $hash ]]; then
+        do_wget -h "$hash" "$url" "$@"
+    else
+        do_wget "$url" "$@"
+    fi
 }
 
 do_strip() {
     local nostrip="x265|x265-numa|ffmpeg|ffprobe|ffplay"
     local file
-    [[ -n $(find "$LOCALDESTDIR"/bin-video -name "mpv.exe.debug") ]] && nostrip+="|mpv"
-    [[ ${*%.exe} != $* || ${*%.dll} != $* ]] && do_print_progress Stripping
+    [[ -f $LOCALDESTDIR/bin-video/mpv.exe.debug ]] && nostrip+="|mpv"
+    [[ "$@" =~ \.(exe|dll|com)$ ]] &&
+        [[ ! "$@" =~ ($nostrip)\.exe$ ]] && do_print_progress Stripping
     for file; do
-        if echo "$file" | grep -qE "\.(exe|dll|com)$" &&
-            echo "$file" | grep -qvE "(${nostrip})\.exe"; then
-            strip -s "$LOCALDESTDIR/$file"
-        elif echo "$file" | grep -qE "x265(|-numa)\.exe"; then
-            strip --strip-unneeded "$LOCALDESTDIR/$file"
+        file=$(file_installed $file)
+        [[ $? = 0 ]] || continue
+        if [[ $file =~ \.(exe|dll|com)$ ]] &&
+            [[ ! $file =~ ($nostrip)\.exe$ ]]; then
+            strip -s "$file"
+        elif [[ $file =~ x265(|-numa)\.exe$ ]]; then
+            strip --strip-unneeded "$file"
         fi
     done
 }
@@ -389,18 +398,21 @@ pc_exists() {
 
 do_install() {
     [[ $1 = dry ]] && local dryrun=y && shift
-    [[ $dryrun ]] && echo install -D "$1" "$(file_installed "$2")" ||
+    if [[ -n $dryrun ]]; then
+        echo install -D "$1" "$(file_installed "$2")"
+    else
         install -D "$1" "$(file_installed "$2")"
+    fi
 }
 
 do_uninstall() {
     [[ $1 = dry ]] && local dry=y && shift
     [[ $1 = q ]] && local quiet=y && shift
     local files=($(files_exist -l "$@"))
-    if [[ $files ]]; then
-        [[ ! quiet ]] && do_print_progress Running uninstall
+    if [[ -n ${files[@]} ]]; then
+        [[ ! $quiet ]] && do_print_progress Running uninstall
         if [[ $dry ]]; then
-            echo "rm -rf ${files[@]}"
+            echo "rm -rf ${files[*]}"
         else
             rm -rf "${files[@]}"
         fi
@@ -624,16 +636,16 @@ mpv_disabled_all() {
 }
 
 mpv_enable() {
-    mpv_disabled $1 && MPV_OPTS=(${MPV_OPTS[@]//--disable-$1/--enable-$1})
+    mpv_disabled "$1" && MPV_OPTS=(${MPV_OPTS[@]//--disable-$1/--enable-$1})
 }
 
 mpv_disable() {
-    mpv_enabled $1 && MPV_OPTS=(${MPV_OPTS[@]//--enable-$1/--disable-$1})
+    mpv_enabled "$1" && MPV_OPTS=(${MPV_OPTS[@]//--enable-$1/--disable-$1})
 }
 
 do_addOption() {
     local option="$1"
-    if ! do_checkForOptions $option; then
+    if ! do_checkForOptions "$option"; then
         FFMPEG_OPTS+=("$option")
     fi
 }
@@ -676,8 +688,8 @@ do_patch() {
                 echo -e "\tPatch couldn't be applied with 'git am'. Continuing without patching."
             fi
         else
-            if patch --dry-run -s -N -p$strip -i "$patch" >/dev/null 2>&1; then
-                patch -s -N -p$strip -i "$patch"
+            if patch --dry-run -s -N -p"$strip" -i "$patch" >/dev/null 2>&1; then
+                patch -s -N -p"$strip" -i "$patch"
             else
                 echo -e "${orange_color}${patch}${reset_color}"
                 echo -e "\tPatch couldn't be applied with 'patch'. Continuing without patching."
@@ -714,10 +726,11 @@ compilation_fail() {
 }
 
 zip_logs() {
-    local failed=$(get_first_subdir)
+    local failed
+    failed="$(get_first_subdir)"
     pushd "$LOCALBUILDDIR" >/dev/null
     rm -f logs.zip
-    7za -mx=9 a logs.zip *.log *.ini *_options.txt -ir!"$failed/*.log" >/dev/null
+    7za -mx=9 a logs.zip ./*.log ./*.ini ./*_options.txt -ir!"$failed/*.log" >/dev/null
     popd >/dev/null
     [[ -f "$LOCALBUILDDIR/logs.zip" ]] &&
         echo "${green_color}Attach $LOCALBUILDDIR/logs.zip to the GitHub issue.${reset_color}"
@@ -835,7 +848,7 @@ do_pacman_install() {
     for pkg in $packages; do
         [[ "$pkg" != "${MINGW_PACKAGE_PREFIX}-"* ]] && pkg="${MINGW_PACKAGE_PREFIX}-${pkg}"
         if /usr/bin/grep -q "^${pkg}$" <(echo "$installed"); then
-            [[ -z $noop ]] && none=y
+            [[ -z $noop ]] && noop=y
             continue
         else
             noop=n
@@ -938,7 +951,6 @@ create_debug_link() {
 
 get_vs_prefix() {
     local vsprefix
-    local programfiles
     local regkey="/HKLM/software/vapoursynth"
     if [[ -n $(find "$LOCALDESTDIR"/bin-video -iname vspipe.exe) ]]; then
         # look for .dlls in bin-video
@@ -1013,3 +1025,5 @@ clean_suite() {
     fi
     popd >/dev/null
 }
+
+((extglob_set)) && shopt -u extglob

commit 680f022434fd0efb8d519aff536a10d9d6fdc6e2

Fix other regressions since c31a91347

build/media-suite_compile.sh
@@ -441,10 +441,11 @@ if [[ $standalone = y ]] && enabled libvorbis &&
     do_autoreconf
     do_uninstall "${_check[@]}"
     [[ -f Makefile ]] && log distclean make distclean
+    extracommands=()
+    enabled libspeex || extracommands+=(--without-speex)
+    [[ $flac = y ]] || extracommands+=(--without-flac)
     do_separate_confmakeinstall audio --disable-ogg123 --disable-vorbiscomment \
-        --disable-vcut --disable-ogginfo \
-        "$(enabled libspeex || echo --without-speex)" \
-        "$([[ $flac = "y" ]] || echo --without-flac)"
+        --disable-vcut --disable-ogginfo "${extracommands[@]}"
     do_checkIfExist "${_check[@]}"
     _to_remove+=($(pwd))
 fi
@@ -457,7 +458,9 @@ if [[ $standalone = y ]] && enabled libopus &&
         "http://downloads.xiph.org/releases/opus/opus-tools-0.1.9.tar.gz"
     do_uninstall "${_check[@]}"
     [[ -f Makefile ]] && log distclean make distclean
-    do_separate_confmakeinstall audio "$([[ $flac = y ]] || echo "--without-flac")"
+    extracommands=()
+    [[ $flac = y ]] || extracommands+=(--without-flac)
+    do_separate_confmakeinstall audio "${extracommands[@]}"
     do_checkIfExist "${_check[@]}"
 fi
 
@@ -691,7 +694,9 @@ if [[ $mplayer = "y" ]] || ! mpv_disabled libass ||
         do_autoreconf
         do_uninstall "${_check[@]}"
         [[ -f Makefile ]] && log "distclean" make distclean
-        do_separate_confmakeinstall "$(enabled_any {lib,}fontconfig && echo --disable-fontconfig)"
+        extracommands=()
+        enabled_any {lib,}fontconfig && extracommands+=(--disable-fontconfig)
+        do_separate_confmakeinstall "${extracommands[@]}"
         do_checkIfExist "${_check[@]}"
     fi
 fi