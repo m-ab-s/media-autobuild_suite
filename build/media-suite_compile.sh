69c287039ce0818da8e5af7035f0d54b9d85cda4
Ricardo Constantino (:RiCON) <wiiaboo@gmail.com>
2016-08-18 18:00:22 +0100

    gpac: remove cherry-pick; merged upstream

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -898,8 +898,6 @@ fi
 
 _check=(libgpac_static.a bin-video/MP4Box.exe)
 if [[ $mp4box = "y" ]] && do_vcs "https://github.com/gpac/gpac.git"; then
-    git fetch origin "pull/606/head"
-    git cherry-pick FETCH_HEAD || git cherry-pick --abort
     do_uninstall include/gpac "${_check[@]}"
     git grep -PIl "\xC2\xA0" | xargs -r sed -i 's/\xC2\xA0/ /g'
     do_separate_conf --static-mp4box
