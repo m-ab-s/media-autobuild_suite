d5a76630c2209eee947f6dd6ae0157d40b7dc9e4
Ricardo Constantino (:RiCON) <wiiaboo@gmail.com>
2016-09-21 18:04:10 +0100

    gcc: update 64-bit to 6.2.0
    Msys2 MinGW GCC still broken.

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -67,12 +67,12 @@ cd_safe "$LOCALBUILDDIR"
 do_getFFmpegConfig "$license"
 do_getMpvConfig
 
-if [[ $bits = 64bit && "$(pacman -Qe "$MINGW_PACKAGE_PREFIX-gcc")" = *"6.1.0-1" &&
-    -f "$MINGW_PREFIX/lib/gcc/$MINGW_CHOST"/6.1.0/libgcc_s.a ]]; then
+if [[ $bits = 64bit && "$(pacman -Qe "$MINGW_PACKAGE_PREFIX-gcc" | awk '{print $2}')" = "6.2.0-1" &&
+    -f "$MINGW_PREFIX/lib/gcc/$MINGW_CHOST"/6.2.0/libgcc_s.a ]]; then
     do_unhide_all_sharedlibs
-    "${curl_opts[@]}" -O "https://i.fsbn.eu/mingw-w64-x86_64-gcc-6.1.0-1-any.pkg.tar.xz"
-    pacman -U --noconfirm "mingw-w64-x86_64-gcc-6.1.0-1-any.pkg.tar.xz"
-    add_to_remove "mingw-w64-x86_64-gcc-6.1.0-1-any.pkg.tar.xz"
+    "${curl_opts[@]}" -O "https://i.fsbn.eu/mingw-w64-x86_64-gcc-6.2.0-1-any.pkg.tar.xz"
+    pacman -U --noconfirm "https://i.fsbn.eu/mingw-w64-x86_64-gcc-6.2.0-1-any.pkg.tar.xz"
+    add_to_remove "https://i.fsbn.eu/mingw-w64-x86_64-gcc-6.2.0-1-any.pkg.tar.xz"
     do_hide_all_sharedlibs
 fi
 
