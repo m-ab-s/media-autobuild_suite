b3696fc68516dddb28cbe601ced403afb623b0ed
Ricardo Constantino <wiiaboo@gmail.com>
2016-10-15 12:16:02 +0100

    gpac: clamp to working commit
    Latest that compiled for me, don't really care to look for the cause.

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -892,7 +892,7 @@ if [[ $ffmpeg != "n" ]] && enabled libmfx &&
 fi
 
 _check=(libgpac_static.a bin-video/MP4Box.exe)
-if [[ $mp4box = "y" ]] && do_vcs "https://github.com/gpac/gpac.git"; then
+if [[ $mp4box = "y" ]] && do_vcs "https://github.com/gpac/gpac.git#commit=ca22280"; then
     do_uninstall include/gpac "${_check[@]}"
     git grep -PIl "\xC2\xA0" | xargs -r sed -i 's/\xC2\xA0/ /g'
     LDFLAGS+=" -L$LOCALDESTDIR/lib -L$MINGW_PREFIX/lib" \
