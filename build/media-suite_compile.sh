9c2c6c969e0e58fa7257211913ec83d90431de02
Ricardo Constantino <wiiaboo@gmail.com>
2017-05-23 16:59:32 +0100

    tesseract: unclamp git commit

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -363,7 +363,7 @@ if [[ $ffmpeg != "no" || $standalone = y ]] && enabled libtesseract; then
     fi
 
     _check=(libtesseract.{,l}a tesseract.pc)
-    if do_vcs "https://github.com/tesseract-ocr/tesseract.git#commit=8bff1e6"; then
+    if do_vcs "https://github.com/tesseract-ocr/tesseract.git"; then
         do_autogen
         _check+=(bin-global/tesseract.exe)
         do_uninstall include/tesseract "${_check[@]}"
@@ -382,6 +382,7 @@ if [[ $ffmpeg != "no" || $standalone = y ]] && enabled libtesseract; then
         fi
         do_checkIfExist
     fi
+    do_addOption --extra-cflags=-fopenmp --extra-libs=-lgomp
 fi
 
 _check=(librubberband.a rubberband.pc rubberband/{rubberband-c,RubberBandStretcher}.h)
