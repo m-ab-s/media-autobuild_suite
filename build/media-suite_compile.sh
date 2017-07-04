6d177226cf40fae884cc4529d0da5175fbbf87c0
Ricardo Constantino <wiiaboo@gmail.com>
2017-07-04 23:17:15 +0100

    libmediainfo: temporary fix for missing cflag

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -849,7 +849,8 @@ if [[ $mediainfo = "y" ]]; then
     if do_vcs "https://github.com/MediaArea/MediaInfoLib" libmediainfo; then
         do_uninstall include/MediaInfo{,DLL} bin-global/libmediainfo-config "${_check[@]}" libmediainfo.la
         sed -i 's|NOT WIN32|UNIX|g' Project/CMake/CMakeLists.txt
-        do_cmakeinstall Project/CMake
+        CXXFLAGS+=" -DUNICODE" \
+        do_cmakeinstall Project/CMake -DBUILD_ZLIB=off -DBUILD_ZENLIB=off
         sed -i 's|libzen|libcurl libzen|' "$LOCALDESTDIR/lib/pkgconfig/libmediainfo.pc"
         do_checkIfExist
     fi
@@ -861,6 +862,7 @@ if [[ $mediainfo = "y" ]]; then
         do_autogen
         do_uninstall "${_check[@]}"
         [[ -f Makefile ]] && log distclean make distclean
+        CXXFLAGS+=" -DUNICODE" \
         do_configure --build="$MINGW_CHOST" --disable-shared --bindir="$LOCALDESTDIR/bin-video" \
             --enable-staticlibs LIBS="$($PKG_CONFIG --libs libmediainfo)"
         do_makeinstall
