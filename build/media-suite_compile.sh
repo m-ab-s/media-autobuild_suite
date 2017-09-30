e706b3a8f7b991a60a3a6e2102323638f95270c8
laserfoxcn <32047881+laserfoxcn@users.noreply.github.com>
2017-09-30 09:38:40 +0800

    FreeType: updated to 2.8.1 to adapt fontconfig
    FreeType: updated to 2.8.1 to adapt fontconfig

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -128,9 +128,9 @@ if [[ "$mplayer" = "y" ]] || ! mpv_disabled libass ||
 
     _check=(libfreetype.{l,}a freetype2.pc)
     [[ $ffmpeg = "sharedlibs" ]] && _check+=(bin-video/libfreetype-6.dll libfreetype.dll.a)
-    if do_pkgConfig "freetype2 = 20.0.14" "2.8.0"; then
-        do_wget_sf -h a3c603ed84c3c2495f9c9331fe6bba3bb0ee65e06ec331e0a0fb52158291b40b \
-            "freetype/freetype2/2.8/freetype-2.8.tar.bz2"
+    if do_pkgConfig "freetype2 = 21.0.15" "2.8.1"; then
+        do_wget_sf -h e5435f02e02d2b87bb8e4efdcaa14b1f78c9cf3ab1ed80f94b6382fb6acc7d78 \
+            "freetype/freetype2/2.8.1/freetype-2.8.1.tar.bz2"
         do_uninstall include/freetype2 bin-global/freetype-config \
             bin{,-video}/libfreetype-6.dll libfreetype.dll.a "${_check[@]}"
         extracommands=(--with-{harfbuzz,png,bzip2}=no)
