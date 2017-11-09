4879eed1f1cc9b5d136f6b9d8d1a3cd38c7f2b2d
Ricardo Constantino <wiiaboo@gmail.com>
2017-11-09 00:46:02 +0000

    fontconfig: add itstool new mandatory dependency
    Closes #629

--- media-autobuild_suite.bat
+++ media-autobuild_suite.bat
@@ -61,7 +61,7 @@ if not exist %build% mkdir %build%
 
 set msyspackages=asciidoc autoconf automake-wrapper autogen bison diffstat dos2unix help2man ^
 intltool libtool patch python xmlto make zip unzip git subversion wget p7zip mercurial man-db ^
-gperf winpty texinfo gyp-git doxygen autoconf-archive
+gperf winpty texinfo gyp-git doxygen autoconf-archive itstool
 
 set mingwpackages=cmake dlfcn libpng gcc nasm pcre tools-git yasm ninja pkg-config lz4
 
