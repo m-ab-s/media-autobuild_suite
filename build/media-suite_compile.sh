m-ab-s/media-autobuild_suite

commit 96770c823078991514e9dc960a9918505c36a422

helper: add way to get custom configure options for some packages

build/media-suite_helper.sh
@@ -620,18 +620,15 @@ do_readoptionsfile() {
     local filename="$1"
     local varname="$2"
     if [[ -f "$filename" ]]; then
-        IFS=$'\n' read -d '' -r -a "$varname" < <(< $filename dos2unix |
+        printf '%s\n' "$(< $filename dos2unix |
             sed -r '# remove commented text
                     s/#.*//
                     # delete empty lines
                     /^\s*$/d
                     # remove leading/trailing whitespace
                     s/(^\s+|\s+$)//
-                    ')
-        echo "Imported options from ${filename##*/}"
-    else
-        echo "Options file not found, using default options"
-        declare -ag "$varname"
+                    ')"
+        echo "Imported options from ${filename##*/}" >&2
     fi
 }
 
@@ -646,6 +643,7 @@ do_readbatoptions() {
 do_getFFmpegConfig() {
     local license="${1:-nonfree}"
 
+    FFMPEG_DEFAULT_OPTS=()
     if [[ -f "/trunk/media-autobuild_suite.bat" && $ffmpegChoice =~ (n|z|f) ]]; then
         IFS=$'\n' read -d '' -r -a bat < <(< /trunk/media-autobuild_suite.bat dos2unix)
         FFMPEG_DEFAULT_OPTS=($(do_readbatoptions "ffmpeg_options_(builtin|basic)"))
@@ -655,7 +653,8 @@ do_getFFmpegConfig() {
             FFMPEG_DEFAULT_OPTS+=($(do_readbatoptions "ffmpeg_options_full"))
         echo "Imported default FFmpeg options from .bat"
     else
-        do_readoptionsfile "${CONFIGFILE:-$LOCALBUILDDIR/ffmpeg_options.txt}" FFMPEG_DEFAULT_OPTS
+        IFS=$'\n' FFMPEG_DEFAULT_OPTS=(
+            $(do_readoptionsfile "$LOCALBUILDDIR/ffmpeg_options.txt"))
     fi
     echo "License: $license"
     FFMPEG_OPTS=("${FFMPEG_BASE_OPTS[@]}" "${FFMPEG_DEFAULT_OPTS[@]}")
@@ -842,6 +841,7 @@ disabled_all() {
 }
 
 do_getMpvConfig() {
+    MPV_OPTS=()
     if [[ -f "/trunk/media-autobuild_suite.bat" && "$ffmpegChoice" =~ (n|z|f) ]]; then
         IFS=$'\n' read -d '' -r -a bat < <(< /trunk/media-autobuild_suite.bat dos2unix)
         MPV_OPTS=($(do_readbatoptions "mpv_options_(builtin|basic)"))
@@ -849,7 +849,7 @@ do_getMpvConfig() {
             MPV_OPTS+=($(do_readbatoptions "mpv_options_full"))
         echo "Imported default mpv options from .bat"
     else
-        do_readoptionsfile "${CONFIGFILE:-$LOCALBUILDDIR/mpv_options.txt}" MPV_OPTS
+        IFS=$'\n' MPV_OPTS=($(do_readoptionsfile "$LOCALBUILDDIR/mpv_options.txt"))
     fi
     do_removeOption MPV_OPTS \
         "--(en|dis)able-(vapoursynth-lazy|libguess|static-build|enable-gpl3|egl-angle-lib)"
@@ -1107,6 +1107,11 @@ create_build_dir() {
     else
         mkdir "$build_dir" && cd_safe "$build_dir"
     fi
+}
+
+get_external_opts() {
+    local pkgname="$(get_first_subdir)"
+    do_readoptionsfile "$LOCALBUILDDIR/${pkgname%-*}_options.txt"
 }
 
 do_separate_conf() {

commit 545b7526235c3ba65cc625970254094d47f49ede

Fix regression caused by 96770c8230

build/media-suite_compile.sh
@@ -766,7 +766,7 @@ if [[ $vpx = y ]] && do_vcs "https://chromium.googlesource.com/webm/libvpx" vpx;
     create_build_dir
     [[ $bits = 32bit ]] && arch=x86 || arch=x86_64
     [[ $ffmpeg = "sharedlibs" ]] || extracommands+=(--enable-{vp9-postproc,vp9-highbitdepth})
-    IFS=$'\n' extracommands+=($(get_external_opts))
+    extracommands+=($(get_external_opts))
     log "configure" ../configure --target="${arch}-win${bits%bit}-gcc" --prefix="$LOCALDESTDIR" \
         --disable-{shared,unit-tests,docs,install-bins} \
         "${extracommands[@]}"
@@ -789,7 +789,7 @@ if [[ $aom = y ]] && do_vcs https://aomedia.googlesource.com/aom; then
     [[ $standalone = y ]] && _check+=(bin-video/aomdec.exe) ||
         extracommands+=(-DENABLE_EXAMPLES=off)
     do_uninstall include/aom "${_check[@]}"
-    IFS=$'\n' extracommands+=($(get_external_opts))
+    extracommands+=($(get_external_opts))
     do_cmakeinstall -DENABLE_{DOCS,TOOLS}=off -DENABLE_NASM=on \
         -DCONFIG_UNIT_TESTS=0 "${extracommands[@]}"
     [[ $standalone = y ]] && mv -f "$LOCALDESTDIR"/bin/aom{enc,dec}.exe \

build/media-suite_helper.sh
@@ -618,16 +618,15 @@ do_pkgConfig() {
 
 do_readoptionsfile() {
     local filename="$1"
-    local varname="$2"
     if [[ -f "$filename" ]]; then
-        printf '%s\n' "$(< $filename dos2unix |
+        < $filename dos2unix |
             sed -r '# remove commented text
                     s/#.*//
                     # delete empty lines
                     /^\s*$/d
                     # remove leading/trailing whitespace
                     s/(^\s+|\s+$)//
-                    ')"
+                    '
         echo "Imported options from ${filename##*/}" >&2
     fi
 }
@@ -653,8 +652,8 @@ do_getFFmpegConfig() {
             FFMPEG_DEFAULT_OPTS+=($(do_readbatoptions "ffmpeg_options_full"))
         echo "Imported default FFmpeg options from .bat"
     else
-        IFS=$'\n' FFMPEG_DEFAULT_OPTS=(
-            $(do_readoptionsfile "$LOCALBUILDDIR/ffmpeg_options.txt"))
+        IFS=$'\n' read -d '' -r -a FFMPEG_DEFAULT_OPTS < \
+            <(do_readoptionsfile "$LOCALBUILDDIR/ffmpeg_options.txt")
     fi
     echo "License: $license"
     FFMPEG_OPTS=("${FFMPEG_BASE_OPTS[@]}" "${FFMPEG_DEFAULT_OPTS[@]}")
@@ -849,7 +848,8 @@ do_getMpvConfig() {
             MPV_OPTS+=($(do_readbatoptions "mpv_options_full"))
         echo "Imported default mpv options from .bat"
     else
-        IFS=$'\n' MPV_OPTS=($(do_readoptionsfile "$LOCALBUILDDIR/mpv_options.txt"))
+        IFS=$'\n' read -d '' -r -a MPV_OPTS < \
+            <(do_readoptionsfile "$LOCALBUILDDIR/mpv_options.txt")
     fi
     do_removeOption MPV_OPTS \
         "--(en|dis)able-(vapoursynth-lazy|libguess|static-build|enable-gpl3|egl-angle-lib)"