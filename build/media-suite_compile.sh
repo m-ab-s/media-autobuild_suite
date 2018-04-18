2398187977175bb2817b11a6e3c00f3d3ae072d7
mkver <mkver@users.noreply.github.com>
2018-04-18 11:13:01 +0200

    nvcodec: fix typo for mpv option check

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -1305,7 +1305,7 @@ fi
 
 _check=(ffnvcodec/nvEncodeAPI.h)
 if [[ $ffmpeg != "no" ]] && { ! disabled_any ffnvcodec autodetect ||
-    ! mpv_disabled cuda_hwaccel; } &&
+    ! mpv_disabled cuda-hwaccel; } &&
     do_vcs "https://git.videolan.org/git/ffmpeg/nv-codec-headers.git"; then
     do_makeinstall PREFIX="$LOCALDESTDIR"
     do_checkIfExist
