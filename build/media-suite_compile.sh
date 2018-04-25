b33fb40382e36c399e62e2d7df1ecbe25f12cea1
Ricardo Constantino <wiiaboo@gmail.com>
2018-04-25 17:37:57 +0100

    tesseract: clamp to working commit

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -370,7 +370,7 @@ if [[ $ffmpeg != "no" || $standalone = y ]] && enabled libtesseract; then
     fi
 
     _check=(libtesseract.{,l}a tesseract.pc)
-    if do_vcs "https://github.com/tesseract-ocr/tesseract.git"; then
+    if do_vcs "https://github.com/tesseract-ocr/tesseract.git#commit=e8fceb58ab2a"; then
         do_autogen
         _check+=(bin-global/tesseract.exe)
         do_uninstall include/tesseract "${_check[@]}"
