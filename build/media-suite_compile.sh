332df1e7aed11df3be913ecc15f81529f2968aa3
Ricardo Constantino <wiiaboo@gmail.com>
2018-06-13 18:41:26 +0100

    libvmaf: clamp to working version
    Likely what FFmpeg will clamp to as well, if they decide to declare vmaf as unstable.

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -1311,7 +1311,7 @@ _check=(libvmaf.{a,h,pc})
 if [[ $bits = 32bit ]]; then
     do_removeOption --enable-libvmaf
 elif [[ $ffmpeg != "no" ]] && enabled libvmaf &&
-    do_vcs "https://github.com/Netflix/vmaf.git"; then
+    do_vcs "https://github.com/Netflix/vmaf.git#tag=v1.3.5"; then
     do_uninstall share/model "${_check[@]}"
     log clean make clean
     do_make INSTALL_PREFIX="$LOCALDESTDIR"
