e9dae4a9687ba389c7688a3be3be24c7e8af8de9
Hubert <schmidthubert@users.noreply.github.com>
2019-03-05 18:43:02 +0100

    tesseract: fix build

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -461,7 +461,7 @@ if [[ $ffmpeg != "no" || $standalone = y ]] && enabled libtesseract; then
         sed -i "s|Libs.private.*|& -lstdc++|" tesseract.pc.in
         do_separate_confmakeinstall global --disable-{graphics,tessdata-prefix} \
             LIBLEPT_HEADERSDIR="$LOCALDESTDIR/include" \
-            LIBS="$($PKG_CONFIG --libs lept libtiff-4)" --datadir="$LOCALDESTDIR/bin-global"
+            LIBS="$($PKG_CONFIG --libs iconv lept libtiff-4)" --datadir="$LOCALDESTDIR/bin-global"
         if [[ ! -f $LOCALDESTDIR/bin-global/tessdata/eng.traineddata ]]; then
             do_pacman_install tesseract-data-eng
             mkdir -p "$LOCALDESTDIR"/bin-global/tessdata
