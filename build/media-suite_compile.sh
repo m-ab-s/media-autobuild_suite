70274641d4eca68f8caead7e58b6f100df5203de
Chris Degawa <ccom@randomderp.com>
2019-05-05 09:12:48 -0500

    VVC: use gitlab mirror

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -1549,7 +1549,7 @@ fi
 
 _check=(bin-video/vvc/{Encoder,Decoder}App.exe)
 if [[ $vvc = y ]] &&
-    do_vcs "https://vcgit.hhi.fraunhofer.de/jvet/VVCSoftware_VTM.git" vvc; then
+    do_vcs "https://gitlab.com/media-autobuild_suite-dependencies/VVCSoftware_VTM.git" vvc; then
     do_uninstall bin-video/vvc
     # patch for easier install of apps
     # probably not of upstream's interest because of how experimental the codec is
