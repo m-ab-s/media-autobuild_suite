m-ab-s/media-autobuild_suite

commit 3f5c7a1051928ceaca5e4fd143de902e19be03cd

Add check for echoing warning
Co-Authored-By: 1480c1 <ccom@randomderp.com>

build/media-suite_compile.sh
@@ -71,7 +71,8 @@ if [[ $noMintty = y ]]; then ( set -o posix ; set )>"$LOCALBUILDDIR/old.var"; fi
 
 source "$LOCALBUILDDIR"/media-suite_helper.sh
 
-echo -e "${orange}Warning: We will not accept any issues lacking any form of logs or logs.zip! ${reset}"
+[[ -f "$LOCALBUILDDIR/no_logs" ||  $logging = n ]]
+    && echo -e "${orange}Warning: We will not accept any issues lacking any form of logs or logs.zip!${reset}"
 
 buildProcess() {
 set_title

commit 8e10814a9921cd9394608c345016253cd0201fc8

compile: fix regression caused by #1190. Fixes #1193

build/media-suite_compile.sh
@@ -71,8 +71,8 @@ if [[ $noMintty = y ]]; then ( set -o posix ; set )>"$LOCALBUILDDIR/old.var"; fi
 
 source "$LOCALBUILDDIR"/media-suite_helper.sh
 
-[[ -f "$LOCALBUILDDIR/no_logs" ||  $logging = n ]]
-    && echo -e "${orange}Warning: We will not accept any issues lacking any form of logs or logs.zip!${reset}"
+[[ -f "$LOCALBUILDDIR/no_logs" ||  $logging = n ]] &&
+    echo -e "${orange}Warning: We will not accept any issues lacking any form of logs or logs.zip!${reset}"
 
 buildProcess() {
 set_title