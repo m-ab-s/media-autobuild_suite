65b9176a7cc0e0f286fed7a34fbe307f6005758e
Ricardo Constantino <wiiaboo@gmail.com>
2019-06-16 16:26:03 +0100

    svthevc: use git am to apply patches

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -1226,9 +1226,9 @@ _check=(SvtHevcEnc.pc libSvtHevcEnc.dll.a svt-hevc/EbApi.h
     bin-video/Svt-Hevc/{libSvtHevcEnc.dll,SvtHevcEncApp.exe})
 if [[ $svthevc = "y" ]] && do_vcs "https://github.com/OpenVisualCloud/SVT-HEVC.git"; then
     do_uninstall bin-video/SVT-Hevc/libSvtHevcEnc.dll.a "${_check[@]}"
-    do_patch "https://github.com/OpenVisualCloud/SVT-HEVC/pull/213.patch"
-    do_patch "https://gist.githubusercontent.com/1480c1/a5fbbff6aa2cb9880275ae6ad63bd9df/raw/0001-transition-between-y4m-and-cmakelists.patch"
-    do_patch "https://github.com/OpenVisualCloud/SVT-HEVC/pull/206.patch"
+    do_patch "https://github.com/OpenVisualCloud/SVT-HEVC/pull/213.patch" am
+    do_patch "https://gist.githubusercontent.com/1480c1/a5fbbff6aa2cb9880275ae6ad63bd9df/raw/0001-transition-between-y4m-and-cmakelists.patch" am
+    do_patch "https://github.com/OpenVisualCloud/SVT-HEVC/pull/206.patch" am
     do_cmakeinstall video -DUNIX=OFF -DNATIVE=OFF
     do_checkIfExist
 fi
