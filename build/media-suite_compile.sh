7a1426ad3370b33befa02bafdf062c88cdbbc7f4
Christopher Degawa <ccom@randomderp.com>
2019-06-21 12:47:02 -0500

    svthevc: remove merged pull request patches

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -1229,8 +1229,6 @@ if [[ $bits = "32bit" ]]; then
 elif { [[ $svthevc = "y" ]] || enabled libsvthevc; } &&
     do_vcs "https://github.com/OpenVisualCloud/SVT-HEVC.git"; then
     do_uninstall "${_check[@]}" include/svt-hevc
-    do_patch "https://github.com/OpenVisualCloud/SVT-HEVC/pull/213.patch" am
-    do_patch "https://gist.githubusercontent.com/1480c1/a5fbbff6aa2cb9880275ae6ad63bd9df/raw/0001-transition-between-y4m-and-cmakelists.patch" am
     do_patch "https://github.com/OpenVisualCloud/SVT-HEVC/pull/206.patch" am
     do_cmakeinstall video -DUNIX=OFF -DNATIVE=OFF
     do_checkIfExist
