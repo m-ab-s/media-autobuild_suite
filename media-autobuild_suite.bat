c76c86366281f94a8663561eb5d83b34a31d2980
Ricardo Constantino <wiiaboo@gmail.com>
2019-08-11 12:15:00 +0100

    .bat: add cuda-llvm as built-in option

--- media-autobuild_suite.bat
+++ media-autobuild_suite.bat
@@ -78,7 +78,7 @@ set mingwpackages=cmake dlfcn libpng gcc nasm pcre tools-git yasm ninja pkg-conf
 
 :: built-ins
 set ffmpeg_options_builtin=--disable-autodetect amf bzlib cuda cuvid d3d11va dxva2 ^
-iconv lzma nvenc schannel zlib sdl2 ffnvcodec nvdec
+iconv lzma nvenc schannel zlib sdl2 ffnvcodec nvdec cuda-llvm
 
 :: common external libs
 set ffmpeg_options_basic=gmp libmp3lame libopus libvorbis libvpx libx264 libx265 ^
