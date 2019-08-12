3edd2fa50b91b0fb0a080c0b524f2a36b92ec838
Christopher Degawa <ccom@randomderp.com>
2019-08-12 11:44:40 +0000

    SVT-HEVC: Remove shared libs

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -1282,8 +1282,8 @@ if [[ $mp4box = "y" ]] && do_vcs "https://github.com/gpac/gpac.git"; then
     do_checkIfExist
 fi
 
-_check=(SvtHevcEnc.pc libSvtHevcEnc.dll.a svt-hevc/EbApi.h
-    bin-video/{libSvtHevcEnc.dll,SvtHevcEncApp.exe})
+_check=(SvtHevcEnc.pc libSvtHevcEnc.a svt-hevc/EbApi.h
+    bin-video/SvtHevcEncApp.exe)
 if [[ $bits = "32bit" ]]; then
     do_removeOption --enable-libsvthevc
 elif { [[ $svthevc = "y" ]] || enabled libsvthevc; } &&
