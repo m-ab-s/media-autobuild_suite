4f457f4d458521cabdb3675229a4e00cea3b94c9
Moises Cardona <moises@moises-studios.com>
2019-08-20 11:32:41 -0400

    Restored the "am" to the patch.
    Co-Authored-By: Christopher Degawa <ccom@randomderp.com>

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -1733,7 +1733,7 @@ if [[ $ffmpeg != "no" ]]; then
         [[ -f ffmpeg_extra.sh ]] && source ffmpeg_extra.sh
 
         if enabled libsvthevc; then
-            do_patch "https://raw.githubusercontent.com/OpenVisualCloud/SVT-HEVC/master/ffmpeg_plugin/0001-lavc-svt_hevc-add-libsvt-hevc-encoder-wrapper.patch" ||
+            do_patch "https://raw.githubusercontent.com/OpenVisualCloud/SVT-HEVC/master/ffmpeg_plugin/0001-lavc-svt_hevc-add-libsvt-hevc-encoder-wrapper.patch" am ||
                 do_removeOption --enable-libsvthevc
         fi
 
