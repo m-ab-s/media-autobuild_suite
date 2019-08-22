4e5cddea86b0baacb44afbfacd27d201226cbe2f
moisesmcardona <moises@moises-studios.com>
2019-08-22 14:52:45 -0400

    Enabled libsvtav1 and placed it next to libsvthevc

--- media-autobuild_suite.bat
+++ media-autobuild_suite.bat
@@ -93,10 +93,9 @@ libaom libopenmpt version3
 
 :: options also available with the suite
 set ffmpeg_options_full=chromaprint decklink frei0r libbs2b libcaca ^
-libcdio libfdk-aac libflite libfribidi libgme libgsm libilbc libsvthevc libkvazaar ^
-libmodplug librtmp librubberband libssh libtesseract libxavs libzmq ^
+libcdio libfdk-aac libflite libfribidi libgme libgsm libilbc libsvthevc libsvtav1 ^
+libkvazaar libmodplug librtmp librubberband libssh libtesseract libxavs libzmq ^
 libzvbi openal libvmaf libcodec2 libsrt ladspa #vapoursynth #liblensfun #librav1e ^
-#libsvtav1
 
 :: options also available with the suite that add shared dependencies
 set ffmpeg_options_full_shared=opencl opengl cuda-nvcc libnpp libopenh264
