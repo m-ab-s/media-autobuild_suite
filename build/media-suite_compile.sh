e69ea4d20297cc43132bd992c6e0a429102a295e
Moises Cardona <moises@moises-studios.com>
2019-09-23 14:24:05 -0400

    Removed merged SVT-AV1 MinGW patch
    Removed SVT-AV1 MinGW patch since it has been merged into SVT-AV1

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -1312,7 +1312,6 @@ if [[ $bits = "32bit" ]]; then
 elif { [[ $svtav1 = y ]] || enabled libsvtav1; } &&
     do_vcs "https://github.com/OpenVisualCloud/SVT-AV1.git"; then
     do_uninstall include/svt-av1 "${_check[@]}" include/svt-av1
-    do_patch "https://patch-diff.githubusercontent.com/raw/OpenVisualCloud/SVT-AV1/pull/558.patch" am
     do_cmakeinstall video -DUNIX=OFF
     do_checkIfExist
 fi
