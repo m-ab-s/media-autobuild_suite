27a53f5e6c7a22be96e126328285bc7b727065d4
Christopher Degawa <ccom@randomderp.com>
2019-09-25 22:08:53 +0000

    bat: Move license after echo off and use rem

--- media-autobuild_suite.bat
+++ media-autobuild_suite.bat
@@ -1,26 +1,26 @@
-::-----------------------------------------------------------------------------
-:: LICENSE --------------------------------------------------------------------
-::-----------------------------------------------------------------------------
-::  This Windows Batchscript is for setup a compiler environment for building
-::  ffmpeg and other media tools under Windows.
-::
-::    Copyright (C) 2013  jb_alvarado
-::
-::    This program is free software: you can redistribute it and/or modify
-::    it under the terms of the GNU General Public License as published by
-::    the Free Software Foundation, either version 3 of the License, or
-::    (at your option) any later version.
-::
-::    This program is distributed in the hope that it will be useful,
-::    but WITHOUT ANY WARRANTY; without even the implied warranty of
-::    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-::    GNU General Public License for more details.
-::
-::    You should have received a copy of the GNU General Public License
-::    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-::-----------------------------------------------------------------------------
-
 @echo off
+rem -----------------------------------------------------------------------------
+rem LICENSE --------------------------------------------------------------------
+rem -----------------------------------------------------------------------------
+rem  This Windows Batchscript is for setup a compiler environment for building
+rem  ffmpeg and other media tools under Windows.
+rem
+rem    Copyright (C) 2013  jb_alvarado
+rem
+rem    This program is free software: you can redistribute it and/or modify
+rem    it under the terms of the GNU General Public License as published by
+rem    the Free Software Foundation, either version 3 of the License, or
+rem    (at your option) any later version.
+rem
+rem    This program is distributed in the hope that it will be useful,
+rem    but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+rem    GNU General Public License for more details.
+rem
+rem    You should have received a copy of the GNU General Public License
+rem    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+rem -----------------------------------------------------------------------------
+
 color 70
 title media-autobuild_suite
 
