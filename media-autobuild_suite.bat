90fa8f4153336aba8c8dcaa5563f6cd099f09fec
Christopher Degawa <ccom@randomderp.com>
2019-11-04 22:17:16 +0000

    bat: Silence findstr on gpg.conf

--- media-autobuild_suite.bat
+++ media-autobuild_suite.bat
@@ -1608,7 +1608,7 @@ rem ------------------------------------------------------------------
 if %build32%==yes call :writeProfile 32
 if %build64%==yes call :writeProfile 64
 
-findstr hkps://keys.openpgp.org "%instdir%\%msys2%\home\%USERNAME%\.gnupg\gpg.conf" || echo keyserver hkps://keys.openpgp.org >> "%instdir%\%msys2%\home\%USERNAME%\.gnupg\gpg.conf"
+findstr hkps://keys.openpgp.org "%instdir%\%msys2%\home\%USERNAME%\.gnupg\gpg.conf" >nul 2>&1 || echo keyserver hkps://keys.openpgp.org >> "%instdir%\%msys2%\home\%USERNAME%\.gnupg\gpg.conf"
 
 rem loginProfile
 if exist %instdir%\%msys2%\etc\profile.pacnew ^
