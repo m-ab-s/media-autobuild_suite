cc6bb9961250f9267ba3dbde63d991cc8629fe2c
moisesmcardona <moises@moises-studios.com>
2020-01-25 09:22:57 -0500

    Removed bash.ps1 from being downloaded since the file has been removed.

--- media-autobuild_suite.bat
+++ media-autobuild_suite.bat
@@ -1299,7 +1299,7 @@ rem ------------------------------------------------------------------
 rem download and install basic msys2 system:
 rem ------------------------------------------------------------------
 cd %build%
-set scripts=media-suite_compile.sh media-suite_helper.sh media-suite_update.sh bash.ps1
+set scripts=media-suite_compile.sh media-suite_helper.sh media-suite_update.sh
 for %%s in (%scripts%) do (
     if not exist "%build%\%%s" (
         powershell -Command (New-Object System.Net.WebClient^).DownloadFile('"https://github.com/m-ab-s/media-autobuild_suite/raw/master/build/%%s"', '"%%s"' ^)
