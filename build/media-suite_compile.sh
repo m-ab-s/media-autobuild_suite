66606291dc900a4f297ae21b8c56ee8c47440dda
Ricardo Constantino <wiiaboo@gmail.com>
2020-02-16 02:04:31 +0000

    libtiff: don't enable for non-standalone libwebp

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -459,7 +459,8 @@ fi
 unset _deps
 
 _check=(libtiff{.a,-4.pc})
-if [[ $ffmpeg != "no" || $standalone = y ]] && enabled_any libtesseract libwebp &&
+if { { [[ $ffmpeg != "no" || $standalone = y ]] && enabled libtesseract; } ||
+    { [[ $standalone = y ]] && enabled libwebp; }; } &&
     do_vcs "https://gitlab.com/libtiff/libtiff.git"; then
     do_pacman_install libjpeg-turbo xz zlib zstd
     do_uninstall "${_check[@]}"
