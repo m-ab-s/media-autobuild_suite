2ee57b8810b3762d4322255316a59f6a98b8d02c
Christopher Degawa <ccom@randomderp.com>
2020-03-10 14:19:12 +0000

    tesseract: install asciidoc-py3-git
    Adds /mingw64/bin/asciidoc
    
    Closes #1555

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -547,7 +547,7 @@ if [[ $ffmpeg != "no" || $standalone = y ]] && enabled libtesseract; then
 
     _check=(libtesseract.{,l}a tesseract.pc)
     if do_vcs "https://github.com/tesseract-ocr/tesseract.git"; then
-        do_pacman_install docbook-xsl libarchive pango
+        do_pacman_install docbook-xsl libarchive pango asciidoc-py3-git
         do_autogen
         _check+=(bin-global/tesseract.exe)
         do_uninstall include/tesseract "${_check[@]}"
