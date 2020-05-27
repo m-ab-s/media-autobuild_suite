4c160296801b47fef424c74117faef6128e5512b
Christopher Degawa <ccom@randomderp.com>
2020-05-27 02:42:46 +0000

    shaderc: Use shinchiro's patch
    Closes #1654

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -2145,6 +2145,10 @@ if [[ $mpv != n ]] && pc_exists libavcodec libavformat libswscale libavfilter; t
         add_third_party "https://github.com/KhronosGroup/SPIRV-Tools.git" spirv-tools
         add_third_party "https://github.com/KhronosGroup/SPIRV-Headers.git" spirv-headers
         add_third_party "https://github.com/KhronosGroup/SPIRV-Cross.git" spirv-cross
+        (
+            cd_safe third_party/glslang
+            do_patch "https://raw.githubusercontent.com/shinchiro/mpv-winbuild-cmake/master/packages/glslang-0001-fix-gcc-10.1-error.patch" am
+        )
 
         # fix python indentation errors from non-existant code review
         grep -ZRlP --include="*.py" '\t' third_party/spirv-tools/ | xargs -r -0 -n1 sed -i 's;\t;    ;g'
