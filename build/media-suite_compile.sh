511cf63d851afe11e92b055103fbaabf56275e52
Christopher Degawa <ccom@randomderp.com>
2020-07-21 15:46:32 +0000

    gpac: Remove merged patch

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -1387,8 +1387,6 @@ fi
 _check=(libgpac_static.a bin-video/MP4Box.exe)
 if [[ $mp4box = y ]] && do_vcs "https://github.com/gpac/gpac.git"; then
     do_uninstall include/gpac "${_check[@]}"
-    do_patch "https://github.com/gpac/gpac/pull/1545.patch" am
-    do_patch "https://github.com/gpac/gpac/pull/1546.patch" am
     git grep -PIl "\xC2\xA0" | xargs -r sed -i 's/\xC2\xA0/ /g'
     LDFLAGS+=" -L$LOCALDESTDIR/lib -L$MINGW_PREFIX/lib" \
         do_separate_conf --static-mp4box
