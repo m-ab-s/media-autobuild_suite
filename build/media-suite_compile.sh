3ca41a4559047bd65ec99ccd6ac9cf6640803cac
Christopher Degawa <ccom@randomderp.com>
2020-08-03 15:06:26 +0000

    vapoursynth: update to 3.8.5 and 51
    Closes #1775

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -1680,7 +1680,7 @@ fi
 
 
 if  { ! mpv_disabled vapoursynth || enabled vapoursynth; }; then
-    _python_ver=3.8.3
+    _python_ver=3.8.5
     _python_lib=python38
     [[ $bits = 32bit ]] && _arch=win32 || _arch=amd64
     _check=("lib$_python_lib.a")
@@ -1693,7 +1693,7 @@ if  { ! mpv_disabled vapoursynth || enabled vapoursynth; }; then
         do_checkIfExist
     fi
 
-    _vsver=50
+    _vsver=51
     _check=(lib{vapoursynth,vsscript}.a vapoursynth{,-script}.pc vapoursynth/{VS{Helper,Script},VapourSynth}.h)
     if pc_exists "vapoursynth = $_vsver" && files_exist "${_check[@]}"; then
         do_print_status "vapoursynth R$_vsver" "$green" "Up-to-date"
