e57877f87abf1a07b79284de257e3cad6bbf7409
Christopher Degawa <ccom@randomderp.com>
2020-10-31 19:10:01 +0000

    zimg: add patch for not inlining math funcs
    Fixes #1842

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -600,6 +600,7 @@ _check=(zimg{.h,++.hpp} libzimg.{,l}a zimg.pc)
 if [[ $ffmpeg != no ]] && enabled libzimg &&
     do_vcs "https://github.com/sekrit-twc/zimg.git"; then
     do_uninstall "${_check[@]}"
+    do_patch "https://raw.githubusercontent.com/m-ab-s/mabs-patches/master/zimg/0001-libm_wrapper-define-__CRT__NO_INLINE-before-math.h.patch" am
     do_autoreconf
     do_separate_confmakeinstall
     do_checkIfExist
