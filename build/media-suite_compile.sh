d4d19bfb95ebfba2936e978ec2a3c94522eecdfe
Christopher Degawa <ccom@randomderp.com>
2020-11-16 04:38:22 +0000

    jpeg-xl: fix flag

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -1115,7 +1115,7 @@ if [[ $jpegxl = y ]] && do_vcs "https://gitlab.com/wg1/jpeg-xl.git"; then
     do_pacman_install lcms2
     log -q "git.submodule" git submodule update --init --recursive
     do_cmake global -D{BUILD_TESTING,JPEGXL_{ENABLE_BENCHMARK,ENABLE_OPENEXR,ENABLE_SKCMS}}=OFF \
-        JPEGXL_{FORCE_SYSTEM_BROTLI,STATIC}=ON
+        -DJPEGXL_{FORCE_SYSTEM_BROTLI,STATIC}=ON
     do_ninja
     do_install tools/{c,d}jxl.exe bin-global/
     do_checkIfExist
