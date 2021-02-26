a4afbe24101ed61b823f443e1f37dd7ed2d5192a
Christopher Degawa <ccom@randomderp.com>
2021-02-26 16:15:20 -0600

    gpac: add NGHTTP2_STATICLIB for static lib

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -1418,6 +1418,7 @@ if [[ $mp4box = y ]] && do_vcs "https://github.com/gpac/gpac.git"; then
     do_uninstall include/gpac "${_check[@]}"
     git grep -PIl "\xC2\xA0" | xargs -r sed -i 's/\xC2\xA0/ /g'
     LDFLAGS+=" -L$LOCALDESTDIR/lib -L$MINGW_PREFIX/lib" \
+    CFLAGS+=" -DNGHTTP2_STATICLIB" \
         do_separate_conf --static-mp4box
     do_make
     log "install" make install-lib
