033665f381392b7d56ab4ef9b8343251db250f6e
Christopher Degawa <ccom@randomderp.com>
2021-08-15 12:16:44 -0500

    Revert "Revert "Revert "librist: use allow_obj_filter"""
    stil broken with clang and gcc
    
    This reverts commit 20b0363123c1ae0a929544de0fe40fc8d01b9482.

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -1715,8 +1715,10 @@ _check=(librist.{a,pc} librist/librist.h)
 [[ $standalone = y ]] && _check+=(bin-global/rist{sender,receiver,2rist,srppasswd}.exe)
 if enabled librist && do_vcs "https://code.videolan.org/rist/librist.git"; then
     do_patch "https://code.videolan.org/1480c1/librist/-/commit/66cedb7b847b02024b3fe38e6165f0bfe3ec98f5.patch" am
+    do_patch "https://code.videolan.org/1480c1/librist/-/commit/7440c5d570f085e35cffefa4227092a372845e1f.patch" am
+    do_patch "https://code.videolan.org/rist/librist/-/merge_requests/176.patch" am
     do_uninstall include/librist "${_check[@]}"
-    extracommands=("-Dallow_obj_filter=true")
+    extracommands=("-Ddisable_json=true")
     [[ $standalone = y ]] || extracommands+=("-Dbuilt_tools=false")
     do_mesoninstall global -Dhave_mingw_pthreads=true -Dtest=false "${extracommands[@]}"
     do_checkIfExist
