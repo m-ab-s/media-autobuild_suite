4b193dd8de338ef3cc144c36d67161ccfde2e714
Christopher Degawa <ccom@randomderp.com>
2021-09-13 19:57:43 -0500

    libMXF-1.0: remove fixed patch

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -2402,7 +2402,6 @@ if [[ $bmx = y ]]; then
 
     _check=(bin-video/MXFDump.exe libMXF-1.0.{{,l}a,pc})
     if do_vcs "https://gitlab.com/m-ab-s/libmxf.git" libMXF-1.0; then
-        do_patch "https://raw.githubusercontent.com/m-ab-s/mabs-patches/master/libmxf/0001-Add-spaces-between-quotes-and-literal.patch" am
         do_autogen
         do_uninstall include/libMXF-1.0 "${_check[@]}"
         do_separate_confmakeinstall video --disable-examples
