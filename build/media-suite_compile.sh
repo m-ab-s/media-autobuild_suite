b2d0a1fc81a57e6f193fc7d68f0f7093b17bed53
Christopher Degawa <ccom@randomderp.com>
2022-01-20 13:46:22 -0600

    amf: replace patch with pr patch

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -1396,7 +1396,7 @@ fi
 _check=(AMF/core/Version.h)
 if [[ $ffmpeg != no ]] && { enabled amf || ! disabled_any autodetect amf; } &&
     do_vcs "https://github.com/GPUOpen-LibrariesAndSDKs/AMF.git"; then
-    do_patch "https://raw.githubusercontent.com/m-ab-s/mabs-patches/master/AMF/0001-Platform.h-tie-typedef-for-HRESULT-to-windows.patch" am
+    do_patch "https://github.com/GPUOpen-LibrariesAndSDKs/AMF/pull/286.patch" am
     do_uninstall include/AMF
     cd_safe amf/public/include
     install -D -p -t "$LOCALDESTDIR/include/AMF/core" core/*.h
