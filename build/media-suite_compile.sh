775ba72986a7299b09b44ce0aeacc204c2557e8a
Christopher Degawa <ccom@randomderp.com>
2022-04-25 23:30:08 -0500

    openmpt: add patch for pkg-config

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -866,6 +866,7 @@ unset _deps
 _check=(libopenmpt.{a,pc})
 if [[ $ffmpeg != no ]] && enabled libopenmpt &&
     do_vcs "https://github.com/OpenMPT/openmpt.git#tag=libopenmpt-*"; then
+    do_patch "https://raw.githubusercontent.com/m-ab-s/mabs-patches/master/openmpt/0001-make-try-using-PKG_CONFIG-if-provided.patch" am
     do_uninstall include/libopenmpt "${_check[@]}"
     mkdir bin 2> /dev/null
     extracommands=("CONFIG=mingw64-win${bits%bit}" "AR=ar" "STATIC_LIB=1" "EXAMPLES=0" "OPENMPT123=0"
