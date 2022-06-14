65a0af772a8b9d4ebb08e1a37a7d5ffabe04193c
Martin Eesmaa <88035011+MartinEesmaa@users.noreply.github.com>
2022-06-14 16:30:27 +1000

    replace bin-video by include, add check
    Co-authored-by: Christopher Degawa <ccom@randomderp.com>

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -1832,7 +1832,7 @@ _check=(bin-video/vvenc{,FF}.exe
     libapputils.a)
 if [[ $bits = 64bit && $vvenc = y ]] &&
     do_vcs "https://github.com/fraunhoferhhi/vvenc.git" vvenc; then
-    do_uninstall bin-video/vvenc
+    do_uninstall include/vvenc lib/cmake/vvenc "${_check[@]}"
     do_cmakeinstall -DCMAKE_INSTALL_BINDIR="$LOCALDESTDIR"/bin-video/vvenc \
         -DBUILD_STATIC=on
     do_checkIfExist
