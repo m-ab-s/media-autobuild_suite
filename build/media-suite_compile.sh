ca6fab3b889257084cf35af6f5ebed9a36598b5d
Christopher Degawa <ccom@randomderp.com>
2022-08-03 13:09:54 -0500

    vapoursynth: update to 59 and python 3.10.6
    Closes #2211

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -1745,8 +1745,8 @@ if enabled librist && do_vcs "https://code.videolan.org/rist/librist.git"; then
 fi
 
 if  { ! mpv_disabled vapoursynth || enabled vapoursynth; }; then
-    _python_ver=3.9.10
-    _python_lib=python39
+    _python_ver=3.10.6
+    _python_lib=python310
     [[ $bits = 32bit ]] && _arch=win32 || _arch=amd64
     _check=("lib$_python_lib.a")
     if files_exist "${_check[@]}"; then
@@ -1758,7 +1758,7 @@ if  { ! mpv_disabled vapoursynth || enabled vapoursynth; }; then
         do_checkIfExist
     fi
 
-    _vsver=57
+    _vsver=59
     _check=(lib{vapoursynth,vsscript}.a vapoursynth{,-script}.pc vapoursynth/{VS{Helper,Script},VapourSynth}.h)
     if pc_exists "vapoursynth = $_vsver" && files_exist "${_check[@]}"; then
         do_print_status "vapoursynth R$_vsver" "$green" "Up-to-date"
