9e95dc6981c18a865c7617a1fb57013ecdc9eab4
Christopher Degawa <ccom@randomderp.com>
2022-09-21 20:52:31 -0500

    vapoursynth: update version numbers
    Closes #2286

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -1744,7 +1744,7 @@ if enabled librist && do_vcs "https://code.videolan.org/rist/librist.git"; then
 fi
 
 if  { ! mpv_disabled vapoursynth || enabled vapoursynth; }; then
-    _python_ver=3.10.6
+    _python_ver=3.10.7
     _python_lib=python310
     [[ $bits = 32bit ]] && _arch=win32 || _arch=amd64
     _check=("lib$_python_lib.a")
@@ -1757,7 +1757,7 @@ if  { ! mpv_disabled vapoursynth || enabled vapoursynth; }; then
         do_checkIfExist
     fi
 
-    _vsver=59
+    _vsver=60
     _check=(lib{vapoursynth,vsscript}.a vapoursynth{,-script}.pc vapoursynth/{VS{Helper,Script},VapourSynth}.h)
     if pc_exists "vapoursynth = $_vsver" && files_exist "${_check[@]}"; then
         do_print_status "vapoursynth R$_vsver" "$green" "Up-to-date"
