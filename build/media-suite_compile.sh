8109519f79e6b431e4ad5ab43a9c9bdc816ca100
Christopher Degawa <ccom@randomderp.com>
2023-07-06 13:59:10 -0500

    vulkan-loader: add patch for cjson

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -1935,6 +1935,7 @@ if { { [[ $ffmpeg != no ]] && enabled_any vulkan libplacebo; } ||
     do_uninstall "${_check[@]}"
     do_patch "$_mabs/vulkan-loader/0001-loader-cross-compile-static-linking-hacks.patch" am
     do_patch "$_mabs/vulkan-loader/0002-pc-remove-CMAKE_CXX_IMPLICIT_LINK_LIBRARIES.patch" am
+    do_patch "$_mabs/vulkan-loader/0003-loader-prefix-cjson-symbols-and-mark-most-as-static.patch" am
     grep_and_sed VULKAN_LIB_SUFFIX loader/vulkan.pc.in \
             's/@VULKAN_LIB_SUFFIX@//'
     create_build_dir
