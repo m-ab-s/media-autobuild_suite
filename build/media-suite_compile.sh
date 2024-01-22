a860c43b6511bd470e50bc2713cfd6d893d6acf9
Christopher Degawa <ccom@randomderp.com>
2024-01-22 17:51:06 -0600

    ffmpeg: add changes for jxl and version includes

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -2170,6 +2170,9 @@ if [[ $ffmpeg != no ]]; then
 
         do_patch "https://raw.githubusercontent.com/m-ab-s/mabs-patches/master/ffmpeg/0001-glslang-Remove-HLSL-and-OGLCompiler-libraries.patch" am
 
+        # Fix for libjxl changes that removes including version.h from decode.h
+        grep_or_sed jxl/version.h libavcodec/libjxl.h 's;#include <jxl/decode.h>;#include <jxl/version.h>\n&;'
+
         _patches=$(git rev-list origin/master.. --count)
         [[ $_patches -gt 0 ]] &&
             do_addOption "--extra-version=g$(git rev-parse --short origin/master)+$_patches"
