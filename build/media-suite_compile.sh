82205907eef1e6d04d7ad4e91bb2f73dcbb92ab7
woot000 <97068837+woot000@users.noreply.github.com>
2024-03-13 13:43:52 -0500

    upx: update to 4.2.2

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -167,9 +167,9 @@ set_title "compiling global tools"
 do_simple_print -p '\n\t'"${orange}Starting $bits compilation of global tools${reset}"
 
 if [[ $packing = y &&
-    ! "$(/opt/bin/upx -V 2> /dev/null | head -1)" = "upx 3.96" ]] &&
-    do_wget -h 014912ea363e2d491587534c1e7efd5bc516520d8f2cdb76bb0aaf915c5db961 \
-        "https://github.com/upx/upx/releases/download/v3.96/upx-3.96-win32.zip"; then
+    ! "$(/opt/bin/upx -V 2> /dev/null | head -1)" = "upx 4.2.2" ]] &&
+    do_wget -h 141e7cd8d009b827590662b482f1ae2f1dda17cf446a5651078235efb1429c59 \
+        "https://github.com/upx/upx/releases/download/v4.2.2/upx-4.2.2-win32.zip"; then
     do_install upx.exe /opt/bin/upx.exe
 fi
 
