b466b09c36b59cb33ee4ad47bf576281d978e8ca
Christopher Degawa <ccom@randomderp.com>
2024-08-15 23:45:38 -0500

    lensfun: remove merged patch

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -1904,7 +1904,6 @@ if [[ $ffmpeg != no ]] && enabled liblensfun; then
     if do_vcs "$SOURCE_REPO_LENSFUN"; then
         do_patch "https://github.com/m-ab-s/mabs-patches/raw/master/lensfun/0001-CMake-exclude-mingw-w64-from-some-msvc-exclusive-thi.patch" am
         do_patch "https://github.com/m-ab-s/mabs-patches/raw/master/lensfun/0002-CMake-don-t-add-glib2-s-includes-as-SYSTEM-dirs.patch" am
-        do_patch "https://github.com/lensfun/lensfun/pull/1999.patch" am
         do_uninstall "bin-video/lensfun" "${_check[@]}"
         CFLAGS+=" -DGLIB_STATIC_COMPILATION" CXXFLAGS+=" -DGLIB_STATIC_COMPILATION" \
             do_cmakeinstall -DBUILD_STATIC=on -DBUILD_{TESTS,LENSTOOL,DOC}=off \
