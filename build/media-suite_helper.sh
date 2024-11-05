cf64df4068fa8357de81fb7a2f3f4d467cf36af1
Christopher Degawa <ccom@randomderp.com>
2024-11-04 23:53:38 -0600

    do_cmake: always export compilation commands
    tells cmake to produce compile_commands.json which can help debugging locally

--- build/media-suite_helper.sh
+++ build/media-suite_helper.sh
@@ -1336,6 +1336,7 @@ do_cmake() {
         return
     # shellcheck disable=SC2086
     log "cmake" cmake "$root" -G Ninja -DBUILD_SHARED_LIBS=off \
+        -DCMAKE_EXPORT_COMPILE_COMMANDS=on \
         -DCMAKE_TOOLCHAIN_FILE="$LOCALDESTDIR/etc/toolchain.cmake" \
         -DCMAKE_INSTALL_PREFIX="$LOCALDESTDIR" -DUNIX=on \
         -DCMAKE_BUILD_TYPE=Release $bindir "$@" "${cmake_extras[@]}"
