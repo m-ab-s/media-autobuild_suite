8646eec65151c4c56347c98f59d8ce83cda0d493
Christopher Degawa <ccom@randomderp.com>
2025-01-21 12:57:43 -0600

    ffmpeg: add patch for svt-av1's changes

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -2316,6 +2316,9 @@ if [[ $ffmpeg != no ]]; then
             do_patch "https://raw.githubusercontent.com/OpenVisualCloud/SVT-VP9/master/ffmpeg_plugin/master-0001-Add-ability-for-ffmpeg-to-run-svt-vp9.patch" am ||
                 do_removeOption --enable-libsvtvp9
         fi
+        if enabled libsvtav1; then
+            do_patch "https://gitlab.com/AOMediaCodec/SVT-AV1/-/raw/master/.gitlab/workflows/linux/ffmpeg_n7_fix.patch"
+        fi
 
         enabled libsvthevc || do_removeOption FFMPEG_OPTS_SHARED "--enable-libsvthevc"
         enabled libsvtav1 || do_removeOption FFMPEG_OPTS_SHARED "--enable-libsvtav1"
