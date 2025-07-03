2274a6ddbd849f12eff96e383c468f1d5af2c750
Christopher Degawa <ccom@randomderp.com>
2025-07-03 10:26:03 -0500

    ffmpeg: change audiotoolbox's regex

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -2472,7 +2472,7 @@ if [[ $ffmpeg != no ]]; then
         # Bypass ffmpeg check for audiotoolbox
         enabled audiotoolbox && do_addOption --extra-libs=-lAudioToolboxWrapper && do_addOption --disable-outdev=audiotoolbox &&
             do_addOption FFMPEG_OPTS_SHARED --extra-libs=-lAudioToolboxWrapper && do_addOption FFMPEG_OPTS_SHARED --disable-outdev=audiotoolbox &&
-            sed -ri "s/check_apple_framework AudioToolbox/check_apple_framework/g" configure
+            sed -ri "s/enabled audiotoolbox && check_apple_framework.*/enable audiotoolbox/g" configure
 
         if enabled openal &&
             pc_exists "openal"; then
