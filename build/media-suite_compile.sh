0b1591de56b253e3af0b65062f8d408cd6db2796
L4cache <42721712+L4cache@users.noreply.github.com>
2026-01-18 21:44:57 +0800

    compile: add some checks for vulkan & shader stuff
    Prefer libglslang if both are not enabled - one less thing to build.
    But prefer libshaderc in case they are both enabled, as FFmpeg suggests.

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -2301,7 +2301,7 @@ fi
 _check=(lib{glslang,OSDependent}.a
         libSPIRV{,-Tools{,-opt,-link,-reduce}}.a glslang/SPIRV/GlslangToSpv.h)
 if { [[ $mpv != n ]] ||
-     { [[ $ffmpeg != no ]] && enabled_any libplacebo libglslang; } } &&
+     { [[ $ffmpeg != no ]] && enabled_any libplacebo libglslang libshaderc vulkan; } } &&
     do_vcs "$SOURCE_REPO_GLSLANG"; then
     do_uninstall libHLSL.a "${_check[@]}"
     sed -i "s|command_output(\['git', 'clone',|command_output(\['git', 'clone', '--filter=tree:0',|" ./update_glslang_sources.py
@@ -2312,7 +2312,7 @@ fi
 
 _check=(shaderc/shaderc.h libshaderc_combined.a)
 if { [[ $mpv != n ]] ||
-     { [[ $ffmpeg != no ]] && enabled libplacebo; } } ||
+     { [[ $ffmpeg != no ]] && enabled_any libplacebo libshaderc; } } ||
      ! mpv_disabled shaderc &&
     do_vcs "$SOURCE_REPO_SHADERC"; then
     do_patch "https://raw.githubusercontent.com/m-ab-s/mabs-patches/master/shaderc/0001-third_party-set-INSTALL-variables-as-cache.patch" am
@@ -2503,6 +2503,10 @@ if [[ $ffmpeg != no ]]; then
             grep_and_sed '__declspec(__dllimport__)' "$MINGW_PREFIX"/include/gmp.h \
                 's|__declspec\(__dllimport__\)||g' "$MINGW_PREFIX"/include/gmp.h
         fi
+
+        enabled vulkan && ! enabled_any libshaderc libglslang && do_addOption --enable-libglslang
+        enabled_all libshaderc libglslang && do_removeOption --enable-libglslang
+        enabled libshaderc && sed -ri 's/(require_pkg_config spirv_library "shaderc) >/\1_combined >/' configure
 
         _patches=$(git rev-list $ff_base_commit.. --count)
         if [[ $_patches -gt 0 ]]; then
