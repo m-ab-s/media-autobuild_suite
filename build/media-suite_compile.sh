726b22a0710c0451c9bb40032a519113f60c8185
KokonoeTooru <52397082+KokonoeTooru@users.noreply.github.com>
2026-02-23 21:35:48 +0700

    build/media-suite_compile.sh: Remove vorbis-tools patch
    Merged Upstream https://gitlab.xiph.org/xiph/vorbis-tools/-/commit/135b83184f39e57f99fcc2a5f2dd89045f6c74d0

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -946,7 +946,6 @@ _check=(bin-audio/ogg{enc,dec}.exe)
 _deps=(ogg.pc vorbis.pc)
 if [[ $standalone = y ]] && enabled libvorbis &&
     do_vcs "$SOURCE_REPO_VORBIS_TOOLS"; then
-    do_patch "https://raw.githubusercontent.com/m-ab-s/mabs-patches/master/vorbis-tools/0001-utf8-add-empty-convert_free_charset-for-Windows.patch" am
     do_patch "https://raw.githubusercontent.com/m-ab-s/mabs-patches/master/vorbis-tools/0002-getopt-just-remove-it.patch" am
     do_autoreconf
     do_uninstall "${_check[@]}"
