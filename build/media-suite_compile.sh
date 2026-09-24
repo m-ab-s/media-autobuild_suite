849c090bef2736765ce1a63b8e4a097ae7d901b9
Ruonan <yu_ruonan@outlook.com>
2026-09-24 18:35:01 +0800

    kvazaar: pass static flag via pkg-config

--- build/media-suite_compile.sh
+++ build/media-suite_compile.sh
@@ -1521,7 +1521,8 @@ if { [[ $other265 = y ]] || { [[ $ffmpeg != no ]] && enabled libkvazaar; }; } &&
         sed -i "s|bin_PROGRAMS = .*||" src/Makefile.in
     CFLAGS+=" -fno-asynchronous-unwind-tables -DKVZ_BIT_DEPTH=10" \
         do_separate_confmakeinstall video
-    [[ $ffmpeg != no ]] && do_addOption --extra-cflags=-DKVZ_STATIC_LIB
+    grep_or_sed 'KVZ_STATIC_LIB' "$LOCALDESTDIR/lib/pkgconfig/kvazaar.pc" \
+    '/^Cflags:/a Cflags.private: -DKVZ_STATIC_LIB'
     do_checkIfExist
 fi
 
